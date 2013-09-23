16743109ebc6650098e92313540a0255388f9f6a
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2013-09-23 12:25:46 -0700

    Delete slackerRestore.dv6.sh

--- slackTweaks/slackerRestore.dv6.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-## settingsRestoration
-
-sudo cp /home/ry/Dropbox/linuxTweaks/slackTweaks/51-synaptics.conf /etc/X11/xorg.conf.d/51-synaptics.conf
-
-cp ~/Dropbox/linuxTweaks/slackTweaks/slackDrive/.asoundrc ~/.asoundrc
