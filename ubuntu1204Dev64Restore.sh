afdd89dd50c93b225be113c800878d7b3d59fd2a
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2013-10-26 15:14:18 -0700

    Update ubuntu1204Dev64Restore.sh

--- ubuntu1204Dev64Restore.sh
+++ ubuntu1204Dev64Restore.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+echo "# my aliases
+alias aptu='sudo apt-get update && sudo apt-get dist-upgrade -y'
+alias aptc='sudo apt-get autoclean && sudo apt-get clean && sudo apt-get autoremove -y'
+alias apti='sudo apt-get install '" >> ~/.bashrc
+
 sudo apt-get update && sudo apt-get dist-upgrade -y
 
 sudo apt-get install build-essential linux-generic git git-core curl htop vim ruby ruby-dev rubygems python-dev python-setuptools python-pip dkms virtualbox mc
@@ -10,7 +15,7 @@ sudo add-apt-repository -y ppa:otto-kesselgulasch/gimp
 sudo add-apt-repository -y "deb http://repository.spotify.com stable non-free"
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 94558F59
 
-sudo apt-get update && sudo apt-get install libreoffice shutter gimp spotify-client
+sudo apt-get update && sudo apt-get install skype libreoffice shutter gimp spotify-client
 
 sudo apt-get autoclean && sudo apt-get clean && sudo apt-get autoremove -y
 
