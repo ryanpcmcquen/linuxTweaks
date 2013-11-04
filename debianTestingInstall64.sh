386246f1053904eabf09e497a7477819618bb30a
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2013-11-04 14:06:45 -0800

    Update debianTestingInstall64.sh

--- debianTestingInstall64.sh
+++ debianTestingInstall64.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 sudo apt-get install curl build-essential linux-headers-amd64 git-all htop vim python-dev python-pip python-apt \
-  ruby-dev rubygems lftp gftp filezilla dkms mc syslinux nfs-common
+  ruby-dev rubygems lftp gftp filezilla dkms mc syslinux nfs-common nfs-kernel-server
 
 ## grab google chrome, dropbox, sublime text, vagrant, skype
 
