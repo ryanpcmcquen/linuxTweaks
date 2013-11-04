b5594122da08b19ac518c95546bc05bca40ee92a
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2013-11-04 10:09:34 -0800

    Update debianTestingInstall64.sh

--- debianTestingInstall64.sh
+++ debianTestingInstall64.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 sudo apt-get install curl build-essential linux-headers-amd64 git-all htop vim python-dev python-pip \
-  ruby-dev rubygems lftp gftp filezilla dkms mc
+  ruby-dev rubygems lftp gftp filezilla dkms mc syslinux
 
 ## grab google chrome, sublime text, vagrant, skype
 
-sudo pip install ansible
+sudo pip install ansible pip-tools
