8538bbb272a349f4853dcf13badca98a8c72cde9
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2013-11-04 09:00:02 -0800

    Update debianTestingInstall64.sh

--- debianTestingInstall64.sh
+++ debianTestingInstall64.sh
@@ -1,1 +1,2 @@
-sudo apt-get install curl build-essential linux-headers-amd64 git htop vim python-dev python-pip ruby-dev rubygems lftp gftp filezilla dkms mc
+sudo apt-get install curl build-essential linux-headers-amd64 git htop vim python-dev python-pip \
+  ruby-dev rubygems lftp gftp filezilla dkms mc
