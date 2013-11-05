3829777be9382452f1dd062e4168227ddea7c5dc
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2013-11-04 16:32:04 -0800

    Update debianTestingInstall64.sh

--- debianTestingInstall64.sh
+++ debianTestingInstall64.sh
@@ -5,4 +5,8 @@ sudo apt-get install curl build-essential linux-headers-amd64 git-all htop vim p
 
 ## grab google chrome, dropbox, sublime text, vagrant, skype
 
+git config --global user.name "Ryan P.C. McQuen"
+git config --global user.email "ryanpcmcquen@gmail.com"
+git config --global credential.helper cache
+
 sudo pip install ansible pip-tools
