acdf4a0b9f8328fa1cee01463140fee0a7a87f90
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2013-11-05 10:32:12 -0800

    Update ubuntu1204Dev64Restore.sh

--- ubuntu1204Dev64Restore.sh
+++ ubuntu1204Dev64Restore.sh
@@ -7,8 +7,11 @@ alias apti='sudo apt-get install '" >> ~/.bashrc
 
 sudo apt-get update && sudo apt-get dist-upgrade -y
 
-sudo apt-get install build-essential linux-generic git git-core curl lftp htop vim gksu dos2unix ruby ruby-dev rubygems \
-  python-dev python-setuptools python-pip python-apt dkms gftp filezilla mc nfs-common nfs-kernel-server rpcbind asciidoc
+sudo apt-get install build-essential autoconf automake autotools-dev dh-make debhelper devscripts fakeroot xutils lintian \
+  pbuilder linux-generic git git-core curl lftp htop vim gksu dos2unix ruby ruby-dev rubygems \
+  python-dev python-setuptools python-pip python-apt dkms gftp filezilla mc \
+  nfs-common nfs-kernel-server rpcbind asciidoc
+
 
 sudo add-apt-repository -y ppa:libreoffice/ppa
 sudo add-apt-repository -y ppa:maarten-baert/simplescreenrecorder
