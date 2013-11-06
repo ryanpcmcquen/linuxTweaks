e569729c4f6632b6c0b30206b85fbacf0e23fe16
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2013-11-06 07:42:41 -0800

    Update debianTestingInstall64.sh

--- debianTestingInstall64.sh
+++ debianTestingInstall64.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 
-sudo apt-get install curl build-essential linux-headers-amd64 git-all htop dos2unix vim python-dev python-pip python-apt \
-  autoconf automake autotools-dev dh-make debhelper devscripts fakeroot xutils lintian pbuilder git-buildpackage \
-  ruby-dev rubygems lftp gftp filezilla dkms mc syslinux nfs-common nfs-kernel-server rpcbind asciidoc
+sudo apt-get install curl build-essential linux-headers-amd64 git-all htop dos2unix vim python-dev python-pip \
+  autoconf automake autotools-dev dh-make debhelper devscripts xutils lintian pbuilder git-buildpackage \
+  ruby-dev rubygems lftp gftp filezilla mc syslinux nfs-kernel-server asciidoc
+
 
 ## grab google chrome, dropbox, sublime text, vagrant, skype
 ## add user with visudo
+
 
 git config --global user.name "Ryan P.C. McQuen"
 git config --global user.email "ryanpcmcquen@gmail.com"
 git config --global credential.helper cache
 
+
 sudo pip install ansible pip-tools
+
+
+su
+touch /etc/apt/sources.list.d/vbox.list
+echo "deb http://download.virtualbox.org/virtualbox/debian wheezy contrib" >> /etc/apt/sources.list.d/vbox.list
+exit
+wget -q http://download.virtualbox.org/virtualbox/debian/oracle_vbox.asc -O- | sudo apt-key add -
 
 
 echo "
