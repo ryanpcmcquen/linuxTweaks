50c2873eec1c861828f45d9df045c5b3d13b1fc4
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2013-11-10 15:25:25 -0800

    Create removeTheBeep.sh

---
+++ debian/removeTheBeep.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+## To remove all console beeps whilst running X you should execute:
+
+xset b off
+
+##This disables the console beep for all programs.
+
+
+##To remove all console beeps whilst running from the system console execute:
+
+setterm -blength 0
+
+## This disables the console beep for all programs, by setting the "beep length" to zero.
+
+## source: http://www.debian-administration.org/articles/110
