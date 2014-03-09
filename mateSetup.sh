7cb20fca3b0815628a5a9a1878563472be1a04e8
Ryan Q <ryan.q@linux.com>
2014-03-09 10:42:20 -0700

    set a sane terminal shortcut

--- mateSetup.sh
+++ mateSetup.sh
@@ -4,7 +4,7 @@ dconf write /org/mate/peripherals-touchpad/tap-to-click false
 dconf write /org/mate/background/show-desktop-icons false
 dconf write /org/mate/background/picture-filename "'/usr/share/wallpapers/slackware-makepkg-not-war-ryanpc.png'"
 dconf write /org/mate/marco/general/side-by-side-tiling true
-dconf write /org/mate/marco/global-keybindings/run-command-terminal "'<Ctrl><Alt>z'"
+dconf write /org/mate/marco/global-keybindings/run-command-terminal "'<Ctrl><Alt>t'"
 dconf write /org/mate/power-manager/idle-dim-battery false
 dconf write /org/mate/power-manager/backlight-battery-reduce false
 
