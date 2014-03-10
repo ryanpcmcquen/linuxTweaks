cb3fdc5566c9e241b21a16ca7eb525d2a9e6e68c
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-10 06:51:12 -0700

    Update gitNumixAndVim.sh

--- gitNumixAndVim.sh
+++ gitNumixAndVim.sh
@@ -1,28 +1,32 @@
 #!/bin/sh
 ## run this as a normal user
 
-sudo wget https://raw.github.com/numixproject/numix/gh-pages/resources/img/bg.png -P /usr/share/wallpapers/;
+sudo wget -N https://raw.github.com/numixproject/numix/gh-pages/resources/img/bg.png -P /usr/share/wallpapers/;
 
-sudo wget https://raw.github.com/ryanpcmcquen/slackpapers/master/slackware-makepkg-not-war-ryanpc.png -P /usr/share/wallpapers/;
-sudo wget https://raw.github.com/ryanpcmcquen/slackpapers/master/Think_Slack.png -P /usr/share/wallpapers/;
-sudo wget https://raw.github.com/ryanpcmcquen/slackpapers/master/Think_White_slack.png -P /usr/share/wallpapers/;
+sudo wget -N https://raw.github.com/ryanpcmcquen/slackpapers/master/slackware-makepkg-not-war-ryanpc.png -P /usr/share/wallpapers/;
+sudo wget -N https://raw.github.com/ryanpcmcquen/slackpapers/master/Think_Slack.png -P /usr/share/wallpapers/;
+sudo wget -N https://raw.github.com/ryanpcmcquen/slackpapers/master/Think_White_slack.png -P /usr/share/wallpapers/;
 
-sudo wget https://raw.github.com/ryanpcmcquen/googleNowWallpapers/master/seattle2.png -P /usr/share/wallpapers/;
+sudo wget -N https://raw.github.com/ryanpcmcquen/googleNowWallpapers/master/seattle2.png -P /usr/share/wallpapers/;
 
-sudo wget https://github.com/ryanpcmcquen/numix-icon-theme-shine/archive/master.zip -P ~/;
-sudo unzip ~/master.zip -df /usr/share/icons/; sudo mv /usr/share/icons/numix-icon-theme-shine-master/Numix-Shine/ /usr/share/icons/;
-sudo rm -rf /usr/share/icons/numix-icon-theme-shine-master/; sudo rm ~/master.zip;
+sudo wget -N https://github.com/ryanpcmcquen/numix-icon-theme-shine/archive/master.zip -P ~/;
+sudo unzip ~/master.zip -df /usr/share/icons/;
+sudo mv /usr/share/icons/numix-icon-theme-shine-master/Numix-Shine/ /usr/share/icons/;
+sudo rm -rf /usr/share/icons/numix-icon-theme-shine-master/;
+sudo rm ~/master.zip;
 
-sudo wget https://github.com/shimmerproject/Numix/archive/master.zip -P ~/;
-sudo unzip ~/master.zip -df /usr/share/themes/; sudo rm ~/master.zip;
+sudo wget -N https://github.com/shimmerproject/Numix/archive/master.zip -P ~/;
+sudo unzip ~/master.zip -df /usr/share/themes/;
+sudo rm ~/master.zip;
 
-mkdir -p ~/.vim/tmp/; wget https://raw2.github.com/ryanpcmcquen/linuxTweaks/master/.vimrc -P ~/;
-wget https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.gitconfig -P ~/;
+mkdir -p ~/.vim/tmp/;
+wget -N https://raw2.github.com/ryanpcmcquen/linuxTweaks/master/.vimrc -P ~/;
+wget -N https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.gitconfig -P ~/;
 
 sudo -s <<EOF;
 mkdir -p /root/.vim/tmp/;
-wget https://raw2.github.com/ryanpcmcquen/linuxTweaks/master/.vimrc -P /root/;
-wget https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.gitconfig -P /root/;
+wget -N https://raw2.github.com/ryanpcmcquen/linuxTweaks/master/.vimrc -P /root/;
+wget -N https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.gitconfig -P /root/;
 exit;
 EOF
 
