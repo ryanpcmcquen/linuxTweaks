24c91b5a829d41d74634376066af373f13167e03
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-10 07:03:00 -0700

    Update gitNumixAndVim.sh

--- gitNumixAndVim.sh
+++ gitNumixAndVim.sh
@@ -11,15 +11,16 @@ sudo wget -N https://raw.github.com/ryanpcmcquen/slackpapers/master/Think_White_
 
 sudo wget -N https://raw.github.com/ryanpcmcquen/googleNowWallpapers/master/seattle2.png -P /usr/share/wallpapers/;
 
-sudo wget -N https://github.com/ryanpcmcquen/numix-icon-theme-shine/archive/master.zip -P ~/;
-sudo unzip ~/master.zip -df /usr/share/icons/;
-sudo mv /usr/share/icons/numix-icon-theme-shine-master/Numix-Shine/ /usr/share/icons/;
-sudo rm -rf /usr/share/icons/numix-icon-theme-shine-master/;
-sudo rm ~/master.zip;
+## doesn't work
+#sudo wget -N https://github.com/ryanpcmcquen/numix-icon-theme-shine/archive/master.zip -P ~/;
+#sudo unzip ~/master.zip -df /usr/share/icons/;
+#sudo mv /usr/share/icons/numix-icon-theme-shine-master/Numix-Shine/ /usr/share/icons/;
+#sudo rm -rf /usr/share/icons/numix-icon-theme-shine-master/;
+#sudo rm ~/master.zip;
 
-sudo wget -N https://github.com/shimmerproject/Numix/archive/master.zip -P ~/;
-sudo unzip ~/master.zip -df /usr/share/themes/;
-sudo rm ~/master.zip;
+#sudo wget -N https://github.com/shimmerproject/Numix/archive/master.zip -P ~/;
+#sudo unzip ~/master.zip -df /usr/share/themes/;
+#sudo rm ~/master.zip;
 
 mkdir -p ~/.vim/tmp/;
 wget -N https://raw2.github.com/ryanpcmcquen/linuxTweaks/master/.vimrc -P ~/;
