514a26ffd31e52e9a5870036258ddcc14edea654
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-16 14:26:50 -0700

    Create bashGitVimNORMAL.sh

---
+++ bashGitVimNORMAL.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget https://raw.github.com/ryanpcmcquen/linuxTweaks/master/slackware/normal/.bashrc -P ~/
+wget https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.gitconfig -P ~/
+wget https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.vimrc -P ~/
+
