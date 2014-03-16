e94de82b02790043d432fd28497beedfa0f401df
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-16 14:29:47 -0700

    Update bashGitVimROOT.sh

--- bashGitVimROOT.sh
+++ bashGitVimROOT.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# curl https://raw.github.com/ryanpcmcquen/linuxTweaks/master/bashGitVimROOT.sh | bash
+
 wget https://raw.github.com/ryanpcmcquen/linuxTweaks/master/slackware/root/.bashrc -P ~/
 wget https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.gitconfig -P ~/
 wget https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.vimrc -P ~/
