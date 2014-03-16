1ad677c29d5b7de2098aca092251d7843407243a
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-16 14:28:12 -0700

    Create bashGitVimROOT.sh

---
+++ bashGitVimROOT.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+wget https://raw.github.com/ryanpcmcquen/linuxTweaks/master/slackware/root/.bashrc -P ~/
+wget https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.gitconfig -P ~/
+wget https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.vimrc -P ~/
+
