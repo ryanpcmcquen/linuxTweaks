332721b5cc10a98515882f6347c34789a5284785
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-19 08:37:03 -0700

    Update bashGitVimNORMAL.sh

--- bashGitVimNORMAL.sh
+++ bashGitVimNORMAL.sh
@@ -3,6 +3,8 @@
 
 # curl https://raw.github.com/ryanpcmcquen/linuxTweaks/master/bashGitVimNORMAL.sh | bash
 
+mkdir -p ~/.vim/tmp
+
 wget -N https://raw.github.com/ryanpcmcquen/linuxTweaks/master/slackware/normal/.bashrc -P ~/
 wget -N https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.gitconfig -P ~/
 wget -N https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.vimrc -P ~/
