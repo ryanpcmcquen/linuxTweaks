4b2ff00a17a75634c0a07dc8d2aa8cf745f9a408
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-20 07:21:26 -0700

    Create restoreFluxbox.sh

---
+++ restoreFluxbox.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+## uxterm
+#sed -i 's/^Mod1 F1 :Exec xterm/Mod1 F1 :Exec uxterm -rv -geometry 140x40' ~/.fluxbox/keys
+
+## terminator
+sed -i 's/^Mod1 F1 :Exec xterm/Mod1 F1 :Exec terminator --geometry 1100x700' ~/.fluxbox/keys
