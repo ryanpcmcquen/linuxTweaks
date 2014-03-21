599f8f8f2d3896ff998fc03f19fdc3747599f0d6
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-21 16:23:32 -0700

    add sbopkg and slackpkg+ download and install

--- slackware/slackFluxConfigROOT.sh
+++ slackware/slackFluxConfigROOT.sh
@@ -10,3 +10,8 @@ sed -i 's/^#[0-9]+_SBo/[0-9]+_SBo/g' /etc/slackpkg/blacklist
 sed -i 's_^# http://mirrors.slackware.com/slackware/slackware64-current/_http://mirrors.slackware.com/slackware/slackware64-current/_g' /etc/slackpkg/mirrors
 
 curl https://raw.github.com/ryanpcmcquen/linuxTweaks/master/bashGitVimROOT.sh | bash
+
+wget -N http://sbopkg.googlecode.com/files/sbopkg-0.37.0-noarch-1_cng.tgz -P ~/
+wget -N http://sourceforge.net/projects/slackpkgplus/files/slackpkg%2B-1.3.0-noarch-2mt.txz -P ~/
+
+installpkg ~/*.t?z
