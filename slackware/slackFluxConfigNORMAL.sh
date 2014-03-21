db13269fc7efbb785edcf37026f66b2aed2fd9a5
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-21 16:21:24 -0700

    add curl commands for other config scripts

--- slackware/slackFluxConfigNORMAL.sh
+++ slackware/slackFluxConfigNORMAL.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+
+
+curl https://raw.github.com/ryanpcmcquen/linuxTweaks/master/bashGitVimNORMAL.sh | bash
+
+curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/restoreFluxbox.sh | bash
+
 wget -N http://box-look.org/CONTENT/content-files/159716-numixpeg.tar.gz -P ~/
 tar xf ~/159716-numixpeg.tar.gz -C ~/.fluxbox/styles
 
