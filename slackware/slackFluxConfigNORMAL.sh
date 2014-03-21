ced5dfe3569aecf7050b786fb129a5fa636a6b80
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-21 16:36:13 -0700

    tighten up spacing

--- slackware/slackFluxConfigNORMAL.sh
+++ slackware/slackFluxConfigNORMAL.sh
@@ -3,11 +3,9 @@
 # curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/slackware/slackFluxConfigNORMAL.sh | bash
 
 curl https://raw.github.com/ryanpcmcquen/linuxTweaks/master/bashGitVimNORMAL.sh | bash
-
 curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/restoreFluxbox.sh | bash
 
 wget -N http://box-look.org/CONTENT/content-files/159716-numixpeg.tar.gz -P ~/
 tar xf ~/159716-numixpeg.tar.gz -C ~/.fluxbox/styles
-
 echo "session.styleFile:  /home/ry/.fluxbox/styles/numixpeg" >> ~/.fluxbox/init
 
