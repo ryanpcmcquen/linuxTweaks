83d52c681696739eadffaf4b0bdc273b6175363a
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-20 20:52:25 -0700

    Update restoreFluxbox.sh

--- restoreFluxbox.sh
+++ restoreFluxbox.sh
@@ -9,8 +9,8 @@
 sed -i 's/^Mod1 F1 :Exec xterm/Mod1 F1 :Exec terminator --geometry 1100x700/g' ~/.fluxbox/keys
 
 
-sed -i 's/^# idesk &/superkey-launch & nm-applet & fbsetbg -l/g' ~/.fluxbox/startup
+sed -i 's/# idesk &/superkey-launch & nm-applet & fbsetbg -l/g' ~/.fluxbox/startup
 
 
 sed -i 's/^session.screen0.workspaceNames: Workspace 1,Workspace 2,Workspace 3,Workspace 4,/session.screen0.workspaceNames: 1,2,3,4,/g' ~/.fluxbox/init
-
+echo "session.screen0.workspaceNames: 1,2,3,4," << ~/.fluxbox/init
