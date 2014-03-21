59c6992d6edaf9c3e0285bdd2d54a3f8cc258bbf
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-21 16:18:23 -0700

    Update slackFluxConfigROOT.sh

--- slackware/slackFluxConfigROOT.sh
+++ slackware/slackFluxConfigROOT.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+## adjust slackpkg blacklist
 sed -i 's/^aaa_elflibs/#aaa_elflibs/g' /etc/slackpkg/blacklist
-
 sed -i 's/^#[0-9]+_SBo/[0-9]+_SBo/g' /etc/slackpkg/blacklist
+
+## choose 64-current mirrorbrain mirror
+sed -i 's_^# http://mirrors.slackware.com/slackware/slackware64-current/_http://mirrors.slackware.com/slackware/slackware64-current/_g' /etc/slackpkg/mirrors
+
