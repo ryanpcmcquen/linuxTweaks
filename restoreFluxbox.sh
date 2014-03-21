1da1dc4019821f94f1ed23a21fa99e57c16f9a7f
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-20 19:09:26 -0700

    Update restoreFluxbox.sh

--- restoreFluxbox.sh
+++ restoreFluxbox.sh
@@ -4,7 +4,7 @@
 #sed -i 's/^Mod1 F1 :Exec xterm/Mod1 F1 :Exec uxterm -rv -geometry 140x40' ~/.fluxbox/keys
 
 ## terminator
-sed -i 's/^Mod1 F1 :Exec xterm/Mod1 F1 :Exec terminator --geometry 1100x700' ~/.fluxbox/keys
+sed -i 's/^Mod1 F1 :Exec xterm/Mod1 F1 :Exec terminator --geometry 1100x700/g' ~/.fluxbox/keys
 
 
-sed -i 's/^# idesk &/\\\n superkey-launch & \\\n nm-applet & \\\n fbsetbg -l' ~/.fluxbox/startup
+sed -i '17isuperkey-launch & nm-applet & fbsetbg -l/g' ~/.fluxbox/startup
