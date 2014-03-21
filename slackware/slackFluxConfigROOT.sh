540cf19a4bc3ce1eb2a328195ff0b0d31d54f534
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-21 16:32:37 -0700

    add /etc/profile.d/lang.sh config

--- slackware/slackFluxConfigROOT.sh
+++ slackware/slackFluxConfigROOT.sh
@@ -2,6 +2,23 @@
 
 # curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/slackware/slackFluxConfigROOT.sh | bash
 
+
+## configure lilo
+sed -i 's/^#compact/
+lba32
+compact
+/g' /etc/lilo.conf
+
+sed -i 's/^timeout = 50/timeout = 5/g' /etc/lilo.conf
+
+lilo
+
+## change to utf-8 encoding
+sed -i 's/^export LANG=en_US/#export LANG=en_US/g' /etc/profile.d/lang.sh
+sed -i 's/^#export LANG=en_US.UTF-8/export LANG=en_US.UTF-8/g' /etc/profile.d/lang.sh
+
+export LANG=en_US
+
 ## adjust slackpkg blacklist
 sed -i 's/^aaa_elflibs/#aaa_elflibs/g' /etc/slackpkg/blacklist
 sed -i 's/^#[0-9]+_SBo/[0-9]+_SBo/g' /etc/slackpkg/blacklist
