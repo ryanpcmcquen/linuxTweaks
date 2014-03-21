469795e901451eb118938a047218dda5f10d0e7c
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-20 20:45:06 -0700

    Update restoreFluxbox.sh

--- restoreFluxbox.sh
+++ restoreFluxbox.sh
@@ -3,8 +3,12 @@
 ## uxterm
 #sed -i 's/^Mod1 F1 :Exec xterm/Mod1 F1 :Exec uxterm -rv -geometry 140x40' ~/.fluxbox/keys
 
-## terminator
+## terminator (thinkpad reso)
 sed -i 's/^Mod1 F1 :Exec xterm/Mod1 F1 :Exec terminator --geometry 1100x700/g' ~/.fluxbox/keys
 
 
 sed -i 's/^# idesk &/superkey-launch & nm-applet & fbsetbg -l/g' ~/.fluxbox/startup
+
+
+sed -i 's/^session.screen0.workspaceNames: Workspace 1,Workspace 2,Workspace 3,Workspace 4,/session.screen0.workspaceNames: 1,2,3,4,/g' ~/.fluxbox/init
+
