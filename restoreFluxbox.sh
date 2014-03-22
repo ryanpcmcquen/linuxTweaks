2614da37aa40d007598d0b603b9b86fdb88172af
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-22 09:36:44 -0700

    Update restoreFluxbox.sh

--- restoreFluxbox.sh
+++ restoreFluxbox.sh
@@ -3,17 +3,17 @@
 # curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/restoreFluxbox.sh | bash
 
 ## uxterm
-sed -i 's_^Mod1\ F1\ \:Exec\ xterm_Mod1\ F1\ \:Exec\ uxterm\ -rv\ -geometry\ 140x40\
-_g' ~/.fluxbox/keys
+sed -i 's|^Mod1\ F1\ \:Exec\ xterm|Mod1\ F1\ \:Exec\ uxterm\ -rv\ -geometry\ 140x40\
+||g' ~/.fluxbox/keys
 
 ## terminator (thinkpad reso)
 #sed -i 's/^Mod1 F1 :Exec xterm/Mod1 F1 :Exec terminator -m/g' ~/.fluxbox/keys
 
 
-sed -i 's/\#\ idesk\ \&/superkey-launch\ \&\
+sed -i 's|\#\ idesk\ \&|superkey-launch\ \&\
 nm-applet\ \&\
 fbsetbg\ -l\
-/g' ~/.fluxbox/startup
+||g' ~/.fluxbox/startup
 
 #sed -i '18inm-applet\ &\
 #' ~/.fluxbox/startup
