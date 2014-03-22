a9f51dcd54ba5ef187164c7ed66d80f7c0c7c3b0
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-22 13:07:18 -0700

    add newline to sbo sed command

--- slackware/config-o-matic/slackFluxConfig#.sh
+++ slackware/config-o-matic/slackFluxConfig#.sh
@@ -17,7 +17,8 @@ sed -i 's/^#export LANG=en_US.UTF-8/export LANG=en_US.UTF-8/g' /etc/profile.d/la
 
 ## adjust slackpkg blacklist
 sed -i 's/^aaa_elflibs/#aaa_elflibs/g' /etc/slackpkg/blacklist
-sed -i 's/^#[0-9]+_SBo/[0-9]+_SBo/g' /etc/slackpkg/blacklist
+sed -i 's/^#[0-9]+_SBo/\
+[0-9]+_SBo/g' /etc/slackpkg/blacklist
 
 ## choose 64-current mirrorbrain mirror
 sed -i 's_^# http://mirrors.slackware.com/slackware/slackware64-current/_http://mirrors.slackware.com/slackware/slackware64-current/_g' /etc/slackpkg/mirrors
