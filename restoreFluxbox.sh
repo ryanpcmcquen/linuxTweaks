c06c40b7835929e67f61dc5fa6a95c2420ce1f90
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-22 12:20:51 -0700

    it works!

--- restoreFluxbox.sh
+++ restoreFluxbox.sh
@@ -3,22 +3,17 @@
 # curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/restoreFluxbox.sh | bash
 
 ## uxterm
-sed -i "s|^Mod1 F1 :Exec xterm|Mod1 F1 :Exec uxterm -rv -geometry 140x40\
-||g" ~/.fluxbox/keys
+sed -i 's/^Mod1 F1 :Exec xterm/Mod1 F1 :Exec uxterm -rv -geometry 140x40\
+/g' ~/.fluxbox/keys
 
 ## terminator (thinkpad reso)
 #sed -i 's/^Mod1 F1 :Exec xterm/Mod1 F1 :Exec terminator -m/g' ~/.fluxbox/keys
 
-sed -i "s|^# idesk &|superkey-launch \&\
+sed -i 's/^# idesk &/superkey-launch \&\
 nm-applet \&\
 fbsetbg -l\
-||g" ~/.fluxbox/startup
+/g' ~/.fluxbox/startup
 
-#sed -i '18inm-applet\ &\
-#' ~/.fluxbox/startup
-
-#sed -i '19ifbsetbg\ -l\
-#' ~/.fluxbox/startup
 
 #sed -i 's/^session.screen0.workspaceNames: Workspace 1,Workspace 2,Workspace 3,Workspace 4,/session.screen0.workspaceNames: 1,2,3,4,/g' ~/.fluxbox/init
 
