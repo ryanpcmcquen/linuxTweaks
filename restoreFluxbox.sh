a700ec005bb4e23686d6747285eea93229d67869
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-22 13:16:21 -0700

    change uxterm geometry

--- restoreFluxbox.sh
+++ restoreFluxbox.sh
@@ -3,7 +3,7 @@
 # curl https://raw.github.com/ryanpcmcquen/linuxTweaks/master/restoreFluxbox.sh | bash
 
 ## uxterm
-sed -i 's/^Mod1 F1 :Exec xterm/Mod1 F1 :Exec uxterm -rv -geometry 140x40\
+sed -i 's/^Mod1 F1 :Exec xterm/Mod1 F1 :Exec uxterm -rv -geometry 200x55\
 /g' ~/.fluxbox/keys
 
 ## terminator (thinkpad reso)
