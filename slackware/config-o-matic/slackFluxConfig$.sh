435b44e415dccabf7ca2ce8830864f2e8223dd78
Ryan P.C. McQuen <ryan.q@linux.com>
2014-03-25 06:53:20 -0700

    change from numix to just dark theme

--- slackware/config-o-matic/slackFluxConfig$.sh
+++ slackware/config-o-matic/slackFluxConfig$.sh
@@ -5,7 +5,12 @@
 curl https://raw.github.com/ryanpcmcquen/linuxTweaks/master/bashGitVimNORMAL.sh | bash
 curl https://raw.github.com/ryanpcmcquen/linuxTweaks/master/restoreFluxbox.sh | bash
 
-wget -N http://box-look.org/CONTENT/content-files/159716-numixpeg.tar.gz -P ~/
-tar xf ~/159716-numixpeg.tar.gz -C ~/.fluxbox/styles
-echo "session.styleFile:  /home/ry/.fluxbox/styles/numixpeg" >> ~/.fluxbox/init
+## numix theme
+#wget -N http://box-look.org/CONTENT/content-files/159716-numixpeg.tar.gz -P ~/
+#tar xf ~/159716-numixpeg.tar.gz -C ~/.fluxbox/styles
+#echo "session.styleFile:  /home/ry/.fluxbox/styles/numixpeg" >> ~/.fluxbox/init
 
+## just dark theme
+wget -N http://box-look.org/CONTENT/content-files/163116-Just.tgz -P ~/
+tar xf ~/163116-Just.tgz -C ~/.fluxbox/styles
+echo "session.styleFile:  /home/ry/.fluxbox/styles/Just" >> ~/.fluxbox/init
