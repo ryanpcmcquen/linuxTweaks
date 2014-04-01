4be129865b5644d53851ea7fb7ea308abb2c9203
Ryan P.C. McQuen <ryan.q@linux.com>
2014-04-01 06:46:58 -0700

    update mirrorplus sed command

--- slackware/config-o-matic/slackFluxConfigROOT.sh
+++ slackware/config-o-matic/slackFluxConfigROOT.sh
@@ -51,9 +51,9 @@ installpkg ~/*.t?z
 sed -i 's/^#PKGS_PRIORITY=( myrepo:.* )/\
 PKGS_PRIORITY=( alienbob-current:.* restricted-current:.* )/g' /etc/slackpkg/slackpkgplus.conf
 
-sed -i 's_^#MIRRORPLUS['zerouno']=http://www.z01.eu/repo-slack/slackware64-current/\
-_#MIRRORPLUS['zerouno']=http://www.z01.eu/repo-slack/slackware64-current/\
+sed -i "s|^#MIRRORPLUS['zerouno']=http://www.z01.eu/repo-slack/slackware64-current/|\
+#MIRRORPLUS['zerouno']=http://www.z01.eu/repo-slack/slackware64-current/\
 MIRRORPLUS['alienbob-current']=http://taper.alienbase.nl/mirrors/people/alien/sbrepos/current/x86_64/\
-MIRRORPLUS['restricted-current']=http://taper.alienbase.nl/mirrors/people/alien/restricted_sbrepos/current/x86_64/_g'
+MIRRORPLUS['restricted-current']=http://taper.alienbase.nl/mirrors/people/alien/restricted_sbrepos/current/x86_64/|g" /etc/slackpkg/slackpkgplus.conf
 
 
