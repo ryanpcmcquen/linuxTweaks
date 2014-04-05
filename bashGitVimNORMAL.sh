7618247d4e87f8acf1984ae40dcb420fad4b559c
Ryan P.C. McQuen <ryan.q@linux.com>
2014-04-04 21:24:07 -0700

    add .bash_profile

--- bashGitVimNORMAL.sh
+++ bashGitVimNORMAL.sh
@@ -6,6 +6,7 @@
 mkdir -p ~/.vim/tmp
 
 wget -N https://raw.github.com/ryanpcmcquen/linuxTweaks/master/slackware/normal/.bashrc -P ~/
+wget -N https://raw.github.com/ryanpcmcquen/linuxTweaks/master/slackware/normal/.bash_profile -P ~/
 wget -N https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.gitconfig -P ~/
 wget -N https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.vimrc -P ~/
 
