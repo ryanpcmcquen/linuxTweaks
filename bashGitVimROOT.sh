ba5b6cfa5c3c9f238e0c325ec9d2e58185d76d91
Ryan P.C. McQuen <ryan.q@linux.com>
2014-04-18 07:19:49 -0700

    comment out synaptic conf, remove vim tmp/

--- bashGitVimROOT.sh
+++ bashGitVimROOT.sh
@@ -3,11 +3,10 @@
 
 # curl https://raw.github.com/ryanpcmcquen/linuxTweaks/master/bashGitVimROOT.sh | bash
 
-mkdir -p ~/.vim/tmp
-
 wget -N https://raw.github.com/ryanpcmcquen/linuxTweaks/master/slackware/root/.bashrc -P ~/
 wget -N https://raw.github.com/ryanpcmcquen/linuxTweaks/master/slackware/root/.bash_profile -P ~/
 wget -N https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.gitconfig -P ~/
 wget -N https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.vimrc -P ~/
 
-wget -N https://raw.github.com/ryanpcmcquen/linuxTweaks/master/51-synaptics.conf -P /etc/X11/xorg.conf.d/
+### this should be somewhere else:
+#wget -N https://raw.github.com/ryanpcmcquen/linuxTweaks/master/51-synaptics.conf -P /etc/X11/xorg.conf.d/
