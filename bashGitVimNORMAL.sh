3090e2c9fa29569de6fa4cdac00fc6879f90e924
Ryan P.C. McQuen <ryan.q@linux.com>
2014-04-18 07:20:22 -0700

    remove $'s, remove vim tmp/

--- bashGitVimNORMAL.sh
+++ bashGitVimNORMAL.sh
@@ -3,12 +3,12 @@
 
 # curl https://raw.github.com/ryanpcmcquen/linuxTweaks/master/bashGitVimNORMAL.sh | bash
 
-$BASHRC="https://raw.github.com/ryanpcmcquen/linuxTweaks/master/slackware/normal/.bashrc"
-$BASHPR="https://raw.github.com/ryanpcmcquen/linuxTweaks/master/slackware/normal/.bash_profile"
-$GITCONF="https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.gitconfig"
-$VIMRC="https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.vimrc"
+BASHRC="https://raw.github.com/ryanpcmcquen/linuxTweaks/master/slackware/normal/.bashrc"
+BASHPR="https://raw.github.com/ryanpcmcquen/linuxTweaks/master/slackware/normal/.bash_profile"
+GITCONF="https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.gitconfig"
+VIMRC="https://raw.github.com/ryanpcmcquen/linuxTweaks/master/.vimrc"
 
-mkdir -p ~/.vim/tmp
+
 
 wget -N $BASHRC -P ~/
 wget -N $BASHPR -P ~/
