b56f87af9f5ea40f02179f40b66e03dd3ad6f306
Ryan P.C. McQuen <ryan.q@linux.com>
2014-04-30 08:25:51 -0700

    comment out non-working setting, add xdg tweaks

--- mateSetup.sh
+++ mateSetup.sh
@@ -3,13 +3,17 @@
 
 # curl https://raw2.github.com/ryanpcmcquen/linuxTweaks/master/mateSetup.sh | bash
 
+xdg-mime default caja-folder-handler.desktop inode/directory
+xdg-mime default caja-folder-handler.desktop application/x-directory
+
 dconf write /org/mate/peripherals-touchpad/scroll-method 2
 dconf write /org/mate/peripherals-touchpad/horiz-scroll-enabled true
 dconf write /org/mate/peripherals-touchpad/tap-to-click false
 dconf write /org/mate/background/show-desktop-icons false
-dconf write /org/mate/background/picture-filename "'/usr/share/wallpapers/slackware-makepkg-not-war-ryanpc.png'"
 dconf write /org/mate/marco/general/side-by-side-tiling true
 dconf write /org/mate/marco/global-keybindings/run-command-terminal "'<Ctrl><Alt>t'"
 dconf write /org/mate/power-manager/idle-dim-battery false
 dconf write /org/mate/power-manager/backlight-battery-reduce false
 
+## don't work
+#dconf write /org/mate/background/picture-filename "'/usr/share/wallpapers/slackware-makepkg-not-war-ryanpc.png'"
