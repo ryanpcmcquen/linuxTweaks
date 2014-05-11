71f8166e18220f1360efb6b11c20d13035465f80
Ryan P.C. McQuen <ryan.q@linux.com>
2014-05-11 16:21:58 -0700

    switch to gsettings instead of dconf

--- mateSetup.sh
+++ mateSetup.sh
@@ -6,14 +6,31 @@
 xdg-mime default caja-folder-handler.desktop inode/directory
 xdg-mime default caja-folder-handler.desktop application/x-directory
 
-dconf write /org/mate/desktop/peripherals/touchpad/scroll-method 2
-dconf write /org/mate/desktop/peripherals/touchpad/horiz-scroll-enabled true
-dconf write /org/mate/desktop/peripherals/touchpad/tap-to-click false
-dconf write /org/mate/background/show-desktop-icons false
-dconf write /org/mate/marco/general/side-by-side-tiling true
-dconf write /org/mate/marco/global-keybindings/run-command-terminal "'<Ctrl><Alt>t'"
-dconf write /org/mate/power-manager/idle-dim-battery false
-dconf write /org/mate/power-manager/backlight-battery-reduce false
+gsettings set org.mate.peripherals-touchpad scroll-method 2
+gsettings set org.mate.peripherals-touchpad horiz-scroll-enabled true
+gsettings set org.mate.peripherals-touchpad tap-to-click false
 
-## don't work
-#dconf write /org/mate/background/picture-filename "'/usr/share/wallpapers/slackware-makepkg-not-war-ryanpc.png'"
+gsettings set org.mate.background show-desktop-icons false
+
+gsettings set org.mate.marco.general side-by-side-tiling true
+
+gsettings set org.mate.marco.global-keybindings run-command-terminal "'<Ctrl><Alt>t'"
+
+gsettings set org.mate.power-manager idle-dim-battery false
+gsettings set org.mate.power-manager backlight-battery-reduce false
+
+
+
+
+#dconf write /org/mate/desktop/peripherals/touchpad/scroll-method 2
+#dconf write /org/mate/desktop/peripherals/touchpad/horiz-scroll-enabled true
+#dconf write /org/mate/desktop/peripherals/touchpad/tap-to-click false
+
+#dconf write /org/mate/background/show-desktop-icons false
+
+#dconf write /org/mate/marco/general/side-by-side-tiling true
+
+#dconf write /org/mate/marco/global-keybindings/run-command-terminal "'<Ctrl><Alt>t'"
+
+#dconf write /org/mate/power-manager/idle-dim-battery false
+#dconf write /org/mate/power-manager/backlight-battery-reduce false
