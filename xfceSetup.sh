7f8ec83fc9e7522fde282290cd5648060f714bdc
Ryan P.C. McQuen <ryan.q@linux.com>
2014-05-12 07:47:34 -0700

    add terminal shortcut

--- xfceSetup.sh
+++ xfceSetup.sh
@@ -7,3 +7,5 @@ xfconf-query -c xfwm4 -p /general/wrap_windows -s false
 xfconf-query -c pointers -p /SynPS2_Synaptics_TouchPad/Properties/Synaptics_Edge_Scrolling -t int -s 0 -t int -s 0 -t int -s 0
 xfconf-query -c pointers -p /SynPS2_Synaptics_TouchPad/Properties/Synaptics_Tap_Action -t int -s 0 -t int -s 0 -t int -s 0 -t int -s 0 -t int -s 0 -t int -s 0 -t int -s 0
 xfconf-query -c pointers -p /SynPS2_Synaptics_TouchPad/Properties/Synaptics_Two-Finger_Scrolling -t int -s 1 -t int -s 1
+
+xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Primary><Alt>t" -n -t string -s /usr/bin/xfce4-terminal
