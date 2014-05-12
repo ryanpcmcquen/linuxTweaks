e3d8364a85362f68cfea73d343610e3daf617475
Ryan P.C. McQuen <ryan.q@linux.com>
2014-05-12 10:36:50 -0700

    add types

--- xfceSetup.sh
+++ xfceSetup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-xfconf-query -c xfwm4 -p /general/snap_to_border -n -s true
-xfconf-query -c xfwm4 -p /general/snap_to_windows -n -s true
-xfconf-query -c xfwm4 -p /general/wrap_windows -n -s false
-xfconf-query -c xfwm4 -p /general/wrap_workspaces -n -s false
+xfconf-query -c xfwm4 -p /general/snap_to_border -n -t bool -s true
+xfconf-query -c xfwm4 -p /general/snap_to_windows -n -t bool -s true
+xfconf-query -c xfwm4 -p /general/wrap_windows -n -t bool -s false
+xfconf-query -c xfwm4 -p /general/wrap_workspaces -n -t bool -s false
 
 xfconf-query -c pointers -p /SynPS2_Synaptics_TouchPad/Properties/Synaptics_Edge_Scrolling -n -t int -s 0 -t int -s 0 -t int -s 0
 xfconf-query -c pointers -p /SynPS2_Synaptics_TouchPad/Properties/Synaptics_Tap_Action -n -t int -s 0 -t int -s 0 -t int -s 0 -t int -s 0 -t int -s 0 -t int -s 0 -t int -s 0
@@ -11,10 +11,10 @@ xfconf-query -c pointers -p /SynPS2_Synaptics_TouchPad/Properties/Synaptics_Two-
 
 xfconf-query -c xfce4-keyboard-shortcuts -p "/commands/custom/<Primary><Alt>t" -n -t string -s /usr/bin/xfce4-terminal
 
-xfconf-query -c xfce4-panel -p /plugins/plugin-1/show-button-title -n -s false
-xfconf-query -c xfce4-panel -p /plugins/plugin-5/show-frame -n -s false
+xfconf-query -c xfce4-panel -p /plugins/plugin-1/show-button-title -n -t bool -s false
+xfconf-query -c xfce4-panel -p /plugins/plugin-5/show-frame -n -t bool -s false
 xfconf-query -c xfce4-panel -p /plugins/plugin-5/mode -n -t int -s 3
-xfconf-query -c xfce4-panel -p /plugins/plugin-6/show-frame -n -s false
+xfconf-query -c xfce4-panel -p /plugins/plugin-6/show-frame -n -t bool -s false
 
-xfconf-query -c xfce4-session -p /general/SaveOnExit -n -s false
+xfconf-query -c xfce4-session -p /general/SaveOnExit -n -t bool -s false
 
