74daaba6a3458e5698866d6583a4ade9632f96c3
Ryan P.C. McQuen <ryan.q@linux.com>
2014-05-12 07:36:20 -0700

    add touchpad settings

--- xfceSetup.sh
+++ xfceSetup.sh
@@ -4,3 +4,6 @@ xfconf-query -c xfwm4 -p /general/snap_to_border -s true
 xfconf-query -c xfwm4 -p /general/snap_to_windows -s true
 xfconf-query -c xfwm4 -p /general/wrap_windows -s false
 
+xfconf-query -c pointers -p /SynPS2_Synaptics_TouchPad/Properties/Synaptics_Edge_Scrolling -t int -s 0 -t int -s 0 -t int -s 0
+xfconf-query -c pointers -p /SynPS2_Synaptics_TouchPad/Properties/Synaptics_Tap_Action -t int -s 0 -t int -s 0 -t int -s 0 -t int -s 0 -t int -s 0 -t int -s 0 -t int -s 0
+xfconf-query -c pointers -p /SynPS2_Synaptics_TouchPad/Properties/Synaptics_Two-Finger_Scrolling -t int -s 1 -t int -s 1
