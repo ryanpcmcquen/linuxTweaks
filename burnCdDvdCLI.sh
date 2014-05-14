c64d5338dcb699e259c4f4c2623c580994735f6e
Ryan P.C. McQuen <ryan.q@linux.com>
2014-05-14 07:25:58 -0700

    Create burnCdDvdCLI.sh

---
+++ burnCdDvdCLI.sh
@@ -0,0 +1,8 @@
+http://www.linuxquestions.org/questions/linuxquestions-org-member-success-stories-23/howto-burn-iso-image-on-cd-using-the-command-line-tool-cdrecord-643827/
+
+http://www.cyberciti.biz/faq/linux-find-out-the-name-of-dvd-cd-rom-writer-blu-ray-device/
+
+dmesg | grep 'cdrom|dvd'
+
+cdrecord -v eject speed=8 dev=/dev/sr0 /path/to/ISO
+
