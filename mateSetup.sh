dcf31e0ccb97448a4fc4043196309e43bdb7acc7
Ryan P.C. McQuen <ryan.q@linux.com>
2014-05-15 07:02:43 -0700

    change marco to Marco, should work now!

--- mateSetup.sh
+++ mateSetup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ## run this as normal user
 
-# curl https://raw2.github.com/ryanpcmcquen/linuxTweaks/master/mateSetup.sh | bash
+# curl https://raw2.github.com/ryanpcmcquen/linuxTweaks/master/mateSetup.sh | sh
 
 xdg-mime default caja-folder-handler.desktop inode/directory
 xdg-mime default caja-folder-handler.desktop application/x-directory
@@ -12,9 +12,9 @@ gsettings set org.mate.peripherals-touchpad tap-to-click false
 
 gsettings set org.mate.background show-desktop-icons false
 
-gsettings set org.mate.marco.general side-by-side-tiling true
+gsettings set org.mate.Marco.general side-by-side-tiling true
 
-gsettings set org.mate.marco.global-keybindings run-command-terminal "'<Ctrl><Alt>t'"
+gsettings set org.mate.Marco.global-keybindings run-command-terminal "'<Ctrl><Alt>t'"
 
 gsettings set org.mate.power-manager idle-dim-battery false
 gsettings set org.mate.power-manager backlight-battery-reduce false
