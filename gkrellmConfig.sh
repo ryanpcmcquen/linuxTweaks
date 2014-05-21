3c7c522597a43c3c3dbab5f375bbb459114e785a
Ryan P.C. McQuen <ryan.q@linux.com>
2014-05-20 23:22:59 -0700

    add sysname

--- gkrellmConfig.sh
+++ gkrellmConfig.sh
@@ -2,6 +2,8 @@
 
 # $ curl https://raw2.github.com/ryanpcmcquen/linuxTweaks/master/gkrellmConfig.sh | sh
 
+sed -i 's/enable_sysname 0/enable_sysname 1/g' ~/.gkrellm2/user-config
+
 sed -i 's/skip_taskbar 0/skip_taskbar 1/g' ~/.gkrellm2/user-config
 sed -i 's/skip_pager 0/skip_pager 1/g' ~/.gkrellm2/user-config
 
