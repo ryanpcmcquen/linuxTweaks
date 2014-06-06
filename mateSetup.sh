848a70972d688fd30e999a3861bbc7339af069b8
Ryan P.C. McQuen <ryan.q@linux.com>
2014-06-05 21:27:11 -0700

    remove dconf cruft

--- mateSetup.sh
+++ mateSetup.sh
@@ -10,8 +10,6 @@ gsettings set org.mate.peripherals-touchpad scroll-method 2
 gsettings set org.mate.peripherals-touchpad horiz-scroll-enabled true
 gsettings set org.mate.peripherals-touchpad tap-to-click false
 
-gsettings set org.mate.background show-desktop-icons false
-
 gsettings set org.mate.Marco.general side-by-side-tiling true
 gsettings set org.mate.Marco.general focus-mode "mouse"
 
@@ -22,16 +20,5 @@ gsettings set org.mate.power-manager backlight-battery-reduce false
 
 
 
+#gsettings set org.mate.background show-desktop-icons false
 
-#dconf write /org/mate/desktop/peripherals/touchpad/scroll-method 2
-#dconf write /org/mate/desktop/peripherals/touchpad/horiz-scroll-enabled true
-#dconf write /org/mate/desktop/peripherals/touchpad/tap-to-click false
-
-#dconf write /org/mate/background/show-desktop-icons false
-
-#dconf write /org/mate/marco/general/side-by-side-tiling true
-
-#dconf write /org/mate/marco/global-keybindings/run-command-terminal "'<Ctrl><Alt>t'"
-
-#dconf write /org/mate/power-manager/idle-dim-battery false
-#dconf write /org/mate/power-manager/backlight-battery-reduce false
