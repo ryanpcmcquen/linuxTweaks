c2784dd18459f8b1708d12e0c9bb5117edf11a50
Ryan P.C. McQuen <ryan.q@linux.com>
2014-06-09 04:05:29 -0700

    Update kdeSetup.sh

--- kdeSetup.sh
+++ kdeSetup.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
 
 kwriteconfig --file kdeglobals --group "KDE-Global GUI Settings" --key "GraphicEffectsLevel" 0
+
+kwriteconfig --file startupconfig --group "ksplashrc KSplash Theme Default" --key "ksplashrc_ksplash_theme" Minimalistic
+
+
+kwriteconfig --file kwinrc --group "Windows" --key "FocusPolicy" FocusFollowsMouse
+
+kwriteconfig --file kwinrc --group "Plugins" --key "kwin4_effect_wobblywindowsEnabled" true
+kwriteconfig --file kwinrc --group "Plugins" --key "kwin4_effect_cubeEnabled" true
+
+
+kwriteconfig --file kwinrc --group "Compositing" --key "UnredirectFullscreen" true
+
+
