22eed6a452d34c48d68d4624da8061dd0a9e47c5
Ryan P.C. McQuen <ryan.q@linux.com>
2014-07-01 08:51:40 -0700

    add font settings

--- kdeSetup.sh
+++ kdeSetup.sh
@@ -9,6 +9,17 @@
 
 kwriteconfig --file kdeglobals --group "KDE-Global GUI Settings" --key "GraphicEffectsLevel" 0
 
+kwriteconfig --file kdeglobals --group "General" --key "desktopFont" "Terminus,9,-1,5,50,0,0,0,0,0"
+kwriteconfig --file kdeglobals --group "General" --key "fixed" "Terminus,9,-1,5,50,0,0,0,0,0"
+kwriteconfig --file kdeglobals --group "General" --key "font" "Terminus,9,-1,5,50,0,0,0,0,0"
+kwriteconfig --file kdeglobals --group "General" --key "menuFont" "Terminus,9,-1,5,50,0,0,0,0,0"
+kwriteconfig --file kdeglobals --group "General" --key "smallestReadableFont" "Terminus,9,-1,5,50,0,0,0,0,0"
+kwriteconfig --file kdeglobals --group "General" --key "taskbarFont" "Terminus,9,-1,5,50,0,0,0,0,0"
+kwriteconfig --file kdeglobals --group "General" --key "toolBarFont" "Terminus,9,-1,5,50,0,0,0,0,0"
+kwriteconfig --file kdeglobals --group "General" --key "activeFont" "Terminus,9,-1,5,50,0,0,0,0,0"
+
+kwriteconfig --file plasma-desktop-appletsrc --group "General" --key "fontTime" "Terminus,12,-1,5,50,0,0,0,0,0"
+
 kwriteconfig --file startupconfig --group "ksplashrc KSplash Theme Default" --key "ksplashrc_ksplash_theme" Minimalistic
 
 kwriteconfig --file ksplashrc --group "KSplash" --key "Theme" Minimalistic
