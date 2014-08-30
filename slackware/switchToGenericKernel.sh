b31a8d75541d21c6baabce7b75a60f486e5880fa
Ryan P.C. McQuen <ryan.q@linux.com>
2014-08-30 11:21:04 -0700

    clear boot path with sed

--- slackware/switchToGenericKernel.sh
+++ slackware/switchToGenericKernel.sh
@@ -6,7 +6,7 @@
 /usr/share/mkinitrd/mkinitrd_command_generator.sh | sh
 
 /usr/share/mkinitrd/mkinitrd_command_generator.sh -l \ >> ~/switchKernel.sh
-ls --color=never /boot/vmlinuz-generic-* | tail -1 >> ~/switchKernel.sh
+ls --color=never /boot/vmlinuz-generic-* | tail -1 | sed s_/boot/v_v_g >> ~/switchKernel.sh
 
 sh ~/switchKernel.sh >> /etc/lilo.conf
 
