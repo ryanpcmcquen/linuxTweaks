e95ac406e044f7b25a3e6ecaaabf94088e5762ff
Ryan P.C. McQuen <ryan.q@linux.com>
2014-08-30 11:13:08 -0700

    add curl command

--- slackware/switchToGenericKernel.sh
+++ slackware/switchToGenericKernel.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 ## run as root
 
+# curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/slackware/switchToGenericKernel.sh | sh
+
 /usr/share/mkinitrd/mkinitrd_command_generator.sh | sh
 
 /usr/share/mkinitrd/mkinitrd_command_generator.sh -l >> ~/switchKernel.sh
