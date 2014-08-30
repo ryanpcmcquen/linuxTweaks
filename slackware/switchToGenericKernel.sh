716e8d6cc3f74cd5b72f2ce2f6ac2ad0c9361c56
Ryan P.C. McQuen <ryan.q@linux.com>
2014-08-30 11:50:39 -0700

    got it!

--- slackware/switchToGenericKernel.sh
+++ slackware/switchToGenericKernel.sh
@@ -3,10 +3,20 @@
 
 # curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/slackware/switchToGenericKernel.sh | sh
 
+#GETKERNELNAME="ls --color=never /boot/vmlinuz-generic-* | tail -1"
+
 /usr/share/mkinitrd/mkinitrd_command_generator.sh | sh
 
-echo "/usr/share/mkinitrd/mkinitrd_command_generator.sh -l \ " >> ~/switchKernel.sh
-ls --color=never /boot/vmlinuz-generic-* | tail -1 >> ~/switchKernel.sh
+#echo "/usr/share/mkinitrd/mkinitrd_command_generator.sh -l \ " >> ~/switchKernel.sh
+#echo -n "$GETKERNELNAME" >> ~/kernel-desc
+
+
+ls --color=never /boot/vmlinuz-generic-* | tail -1 > ~/switchKernel.sh
+echo "/usr/share/mkinitrd/mkinitrd_command_generator.sh -l $(cat switchKernel.sh)" > ~/switchKernel.sh
+
+#echo "/usr/share/mkinitrd/mkinitrd_command_generator.sh -l " && $(cat switchKernel.sh) > ~/switchKernel.sh
+#echo '/usr/share/mkinitrd/mkinitrd_command_generator.sh -l '$'\n'"$(cat switchKernel.sh" > ~/switchKernel.sh
+
 
 sh ~/switchKernel.sh >> /etc/lilo.conf
 
