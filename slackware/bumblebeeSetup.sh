c4fe6fdba1f3256d001d493cd38b6889d1245469
Ryan P.C. McQuen <ryan.q@linux.com>
2014-09-04 07:10:38 -0700

    Create bumblebeeSetup.sh

---
+++ slackware/bumblebeeSetup.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+## run as root
+
+cd
+git clone https://github.com/WhiteWolf1776/Bumblebee-SlackBuilds.git
+
+cd Bumblebee-SlackBuilds/
+./download.sh
+groupadd bumblebee
+usermod -G bumblebee -a ry
+usermod -G bumblebee -a root
+cd libbsd/
+./libbsd.SlackBuild
+upgradepkg --install-new /tmp/libbsd-*_bbsb.txz
+cd ../bumblebee/
+./bumblebee.SlackBuild
+upgradepkg --install-new /tmp/bumblebee-*_bbsb.txz
+cd ../bbswitch/
+./bbswitch.SlackBuild
+upgradepkg --install-new /tmp/bbswitch-*_bbsb.txz
+cd ../primus/
+COMPAT32=yes ./primus.SlackBuild
+upgradepkg --install-new /tmp/primus-*_bbsb.txz
+cd ../nouveau-blacklist/
+upgradepkg xf86-video-nouveau-blacklist-noarch-1.txz
+echo xf86-video-nouveau >> /etc/slackpkg/blacklist
+echo "[0-9]+_bbsb" >> /etc/slackpkg/blacklist
+cd ../libvdpau/
+./libvdpau.SlackBuild
+upgradepkg --install-new /tmp/libvdpau-*_bbsb.txz
+cd ../nvidia-kernel/
+COMPAT32=yes ./nvidia-kernel.SlackBuild
+upgradepkg --install-new /tmp/nvidia-kernel-*_bbsb.txz
+cd ../nvidia-bumblebee/
+COMPAT32=yes ./nvidia-bumblebee.SlackBuild
+upgradepkg --install-new /tmp/nvidia-bumblebee-*_bbsb.txz
+chmod +x /etc/rc.d/rc.bumblebeed
+/etc/rc.d/rc.bumblebeed start
+
+echo "if [ -x /etc/rc.d/rc.bumblebeed ]; then
+  /etc/rc.d/rc.bumblebeed start
+fi" >> /etc/rc.d/rc.local
+echo "if [ -x /etc/rc.d/rc.bumblebeed ]; then
+  /etc/rc.d/rc.bumblebeed stop
+fi" >> /etc/rc.d/rc.local_shutdown
+
