735f23741f4e863c22a750b509fe12cd96767ad4
Ryan P.C. McQuen <ryan.q@linux.com>
2014-09-05 16:17:49 -0700

    remove sed command that is now part of c-o-m

--- slackware/sagerScript.sh
+++ slackware/sagerScript.sh
@@ -8,9 +8,9 @@ EOF
   exit 1
 fi
 
+## now part of config-o-matic
+#sed -i 's/^append=" vt.default_utf8=[0-9]"/append=" vt.default_utf8=1 acpi_osi=linux acpi_backlight=vendor"/g' /etc/lilo.conf
 
-sed -i 's/^append=" vt.default_utf8=[0-9]"/append=" vt.default_utf8=1 acpi_osi=linux acpi_backlight=vendor"/g' /etc/lilo.conf
-
-
-curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/slackware/bumblebeeSetup.sh | sh
+## multilib
+curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/slackware/bumblebeeMultilibSetup.sh | sh
 
