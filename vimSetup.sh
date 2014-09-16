3bdd294bb45edbcdf1de7e2911e8633496cc05b7
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2014-09-16 10:16:42 -0700

    Create vimSetup.sh

---
+++ vimSetup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+
+wget -N https://raw2.github.com/ryanpcmcquen/linuxTweaks/master/.vimrc -P ~/
+
+mkdir -p ~/.vim/colors/
+wget -N https://raw.githubusercontent.com/ryanpcmcquen/vim-plain/master/colors/elvis.vim -P ~/.vim/colors/
