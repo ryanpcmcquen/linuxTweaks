3631e6e425fb4f1050f968cfb45c0a11e02e0d52
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2014-09-24 20:12:59 -0700

    adds group, works for recompiling too!

--- slackware/crazybee.sh
+++ slackware/crazybee.sh
@@ -10,6 +10,14 @@ EOF
   exit 1
 fi
 
+if [ ! -z "$( ls ~/Bumblebee-SlackBuilds/ )" ]; then
+  cat << EOF
+You already have the bumblee directory,
+please rename to avoid changes you may have made.
+EOF
+  exit 1
+fi
+
 if [ ! -z "$(ls /var/log/packages/ | grep compat32 )" ]; then
   export COMPAT32=yes;
 fi
@@ -19,41 +27,47 @@ git clone https://github.com/WhiteWolf1776/Bumblebee-SlackBuilds.git
 
 cd Bumblebee-SlackBuilds/
 ./download.sh
+
 groupadd bumblebee
-usermod -G bumblebee -a ry
-usermod -G bumblebee -a root
+cat /etc/passwd | grep "/home" | cut -d: -f1 | xargs -i usermod -G bumblebee -a {}
+
 cd libbsd/
 ./libbsd.SlackBuild
-ls --color=never /tmp/libbsd-*_bbsb.txz | tail -1 | xargs -i upgradepkg --install-new {}
+ls --color=never /tmp/libbsd-*_bbsb.txz | tail -1 | xargs -i upgradepkg --reinstall --install-new {}
+
 cd ../bumblebee/
 ./bumblebee.SlackBuild
-ls --color=never /tmp/bumblebee-*_bbsb.txz | tail -1 | xargs -i upgradepkg --install-new {}
+ls --color=never /tmp/bumblebee-*_bbsb.txz | tail -1 | xargs -i upgradepkg --reinstall --install-new {}
+
 cd ../bbswitch/
 ./bbswitch.SlackBuild
-ls --color=never /tmp/bbswitch-*_bbsb.txz | tail -1 | xargs -i upgradepkg --install-new {}
+ls --color=never /tmp/bbswitch-*_bbsb.txz | tail -1 | xargs -i upgradepkg --reinstall --install-new {}
+
 cd ../primus/
-#COMPAT32=yes ./primus.SlackBuild
 ./primus.SlackBuild
-ls --color=never /tmp/primus-*_bbsb.txz | tail -1 | xargs -i upgradepkg --install-new {}
+ls --color=never /tmp/primus-*_bbsb.txz | tail -1 | xargs -i upgradepkg --reinstall --install-new {}
+
 cd ../nouveau-blacklist/
-upgradepkg xf86-video-nouveau-blacklist-noarch-1.txz
+upgradepkg --reinstall xf86-video-nouveau-blacklist-noarch-1.txz
 if [ -z "$( cat /etc/slackpkg/blacklist | grep xf86-video-nouveau )" ]; then
   echo xf86-video-nouveau >> /etc/slackpkg/blacklist
 fi
-if [ -z "$( cat /etc/slackpkg/blacklist | grep bbsb )" ]; then
+if [ -z "$( cat /etc/slackpkg/blacklist | grep _bbsb )" ]; then
   echo "[0-9]+_bbsb" >> /etc/slackpkg/blacklist
 fi
+
 cd ../libvdpau/
 ./libvdpau.SlackBuild
-ls --color=never /tmp/libvdpau-*_bbsb.txz | tail -1 | xargs -i upgradepkg --install-new {}
+ls --color=never /tmp/libvdpau-*_bbsb.txz | tail -1 | xargs -i upgradepkg --reinstall --install-new {}
+
 cd ../nvidia-kernel/
-#COMPAT32=yes ./nvidia-kernel.SlackBuild
 ./nvidia-kernel.SlackBuild
-ls --color=never /tmp/nvidia-kernel-*_bbsb.txz | tail -1 | xargs -i upgradepkg --install-new {}
+ls --color=never /tmp/nvidia-kernel-*_bbsb.txz | tail -1 | xargs -i upgradepkg --reinstall --install-new {}
+
 cd ../nvidia-bumblebee/
-#COMPAT32=yes ./nvidia-bumblebee.SlackBuild
 ./nvidia-bumblebee.SlackBuild
-ls --color=never /tmp/nvidia-bumblebee-*_bbsb.txz | tail -1 | xargs -i upgradepkg --install-new {}
+ls --color=never /tmp/nvidia-bumblebee-*_bbsb.txz | tail -1 | xargs -i upgradepkg --reinstall --install-new {}
+
 chmod +x /etc/rc.d/rc.bumblebeed
 /etc/rc.d/rc.bumblebeed start
 
