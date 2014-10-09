b25c463488d2174a2742eda36d65c6af06e6887b
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2014-10-08 20:22:09 -0700

    make it way cooler

--- setMAKEOPTS.sh
+++ setMAKEOPTS.sh
@@ -1,6 +1,17 @@
 #!/bin/sh
 ## add this in /etc/profile to make it permanent  ;^)
 
-if [ "$( grep -c ^processor /proc/cpuinfo )" != 1 ]; then
-  export MAKEOPTS="-j$( expr $( grep -c ^processor /proc/cpuinfo ) / 2 )"
+## awesome way
+if [ "$( nproc )" != 1 ]; then
+  export MAKEOPTS="-j$( expr $( nproc ) / 2 )"
 fi
+
+
+## semi-awesome way
+#if [ "$( grep -c ^processor /proc/cpuinfo )" != 1 ]; then
+  #export MAKEOPTS="-j$( expr $( grep -c ^processor /proc/cpuinfo ) / 2 )"
+#fi
+
+## in case you need to check your work
+#echo $MAKEOPTS
+
