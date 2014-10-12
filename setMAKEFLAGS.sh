0701b5001157bd88bde969e7b6fb2bb7c26b8e33
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2014-10-12 08:01:23 -0700

    make nproc check better

--- setMAKEFLAGS.sh
+++ setMAKEFLAGS.sh
@@ -1,33 +1,35 @@
 #!/bin/sh
 ## add this in /etc/profile to make it permanent  ;^)
 
-## half the cores
-if [ -z "$( cat /etc/profile | grep 'MAKEFLAGS' )" ]; then
-  echo >> /etc/profile
-  echo 'if [ "$( nproc )" != 1 ]; then' >> /etc/profile
-  echo '  export MAKEFLAGS="-j$( expr $( nproc ) / 2 )"' >> /etc/profile
-  echo 'fi' >> /etc/profile
-  echo >> /etc/profile
-fi
 
-## cores--
 if [ -z "$( cat /etc/profile | grep 'MAKEFLAGS' )" ]; then
   echo >> /etc/profile
-  echo 'if [ "$( nproc )" != 1 ]; then' >> /etc/profile
+  echo 'if (( $( nproc ) > 2 )); then' >> /etc/profile
+  ## cores--
   echo '  export MAKEFLAGS="-j$( expr $( nproc ) - 1 )"' >> /etc/profile
+  ## half the cores
+  #echo '  export MAKEFLAGS="-j$( expr $( nproc ) / 2 )"' >> /etc/profile
+  echo 'else' >> /etc/profile
+  echo '  export MAKEFLAGS="-j1"' >> /etc/profile
   echo 'fi' >> /etc/profile
   echo >> /etc/profile
 fi
 
 
 ## awesome way
-#if [ "$( nproc )" != 1 ]; then
-#  export MAKEFLAGS="-j$( expr $( nproc ) / 2 )"
+#if (( $( nproc ) > 2 )); then
+#  export MAKEFLAGS="-j$( expr $( nproc ) - 1 )"
+#else
+#  export MAKEFLAGS="-j1"
 #fi
+#echo $MAKEFLAGS
+
 
 ## semi-awesome way
-#if [ "$( grep -c ^processor /proc/cpuinfo )" != 1 ]; then
-  #export MAKEFLAGS="-j$( expr $( grep -c ^processor /proc/cpuinfo ) / 2 )"
+#if (( $( grep -c ^processor /proc/cpuinfo ) > 2 )); then
+#  export MAKEFLAGS="-j$( expr $( grep -c ^processor /proc/cpuinfo ) - 1 )"
+#else
+#  export MAKEFLAGS="-j1"
 #fi
 
 ## in case you need to check your work
