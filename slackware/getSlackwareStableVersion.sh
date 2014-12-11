8fd96a90cefe04806f18216fc51b504b6929dc42
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2014-12-11 11:28:36 -0800

    use www. version (no 301); use more explicit paths

--- slackware/getSlackwareStableVersion.sh
+++ slackware/getSlackwareStableVersion.sh
@@ -6,11 +6,11 @@ cd
 
 ## get stable version number
 
-wget slackware.com -O slackware-home-page.html -P ~/
+wget www.slackware.com -O slackware-home-page.html -P ~/
 
-cat slackware-home-page.html | grep "is released!" | head -1 | sed 's/Slackware//g' | sed 's/is released!//g' | sed 's/ //g' > ~/SlackwareStableVersion
+cat ~/slackware-home-page.html | grep "is released!" | head -1 | sed 's/Slackware//g' | sed 's/is released!//g' | sed 's/ //g' > ~/SlackwareStableVersion
 
-rm slackware-home-page.html
+rm ~/slackware-home-page.html
 
 
 export SLACKSTAVER=${SLACKSTAVER="$( tr -d '\n\r' < ~/SlackwareStableVersion )"}
