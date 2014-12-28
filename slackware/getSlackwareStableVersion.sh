b17f2eb2e0fa3d46a9ff02a82589065a188c2ab5
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2014-12-28 09:37:52 -0800

    sync with c-o-m version

--- slackware/getSlackwareStableVersion.sh
+++ slackware/getSlackwareStableVersion.sh
@@ -1,21 +1,12 @@
 #!/bin/sh
-
 ## versions!
-
 cd
-
-## get stable version number
-
+## get stable slackware version
 wget www.slackware.com -O ~/slackware-home-page.html
+cat ~/slackware-home-page.html | grep "is released!" | head -1 | sed 's/Slackware//g' | \
+  sed 's/is released!//g' | sed 's/ //g' > ~/slackwareStableVersion
+rm -v ~/slackware-home-page.html
 
-cat ~/slackware-home-page.html | grep "is released!" | head -1 | sed 's/Slackware//g' | sed 's/is released!//g' | sed 's/ //g' > ~/SlackwareStableVersion
-
-rm ~/slackware-home-page.html
-
-
-export SLACKSTAVER=${SLACKSTAVER="$( tr -d '\n\r' < ~/SlackwareStableVersion )"}
-
-export DASHSLACKSTAVER=${DASHSLACKSTAVER=-"$( tr -d '\n\r' < ~/SlackwareStableVersion )"}
-
-
+export SLACKSTAVER=${SLACKSTAVER="$( tr -d '\n\r' < ~/slackwareStableVersion )"}
+export DASHSLACKSTAVER=${DASHSLACKSTAVER=-"$( tr -d '\n\r' < ~/slackwareStableVersion )"}
 
