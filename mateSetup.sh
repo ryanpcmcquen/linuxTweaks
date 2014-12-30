f441453127981788026608482e182f9a8ad3a160
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2014-12-30 07:21:45 -0800

    you can change the cursor shape!

--- mateSetup.sh
+++ mateSetup.sh
@@ -20,21 +20,24 @@ gsettings set org.mate.power-manager backlight-battery-reduce false
 
 gsettings set org.mate.background show-desktop-icons true
 
-##terminal settings
+## terminal settings
 gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ default-size-columns 140
 gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ default-size-rows 40
 gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ use-custom-default-size true
 gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ scrollback-unlimited true
 
-## solarized?
-PALETTE="#070736364242:#D3D301010202:#858599990000:#B5B589890000:#26268B8BD2D2:#D3D336368282:#2A2AA1A19898:#EEEEE8E8D5D5:#00002B2B3636:#CBCB4B4B1616:#58586E6E7575:#65657B7B8383:#838394949696:#6C6C7171C4C4:#9393A1A1A1A1:#FDFDF6F6E3E3"
-BG_COLOR="#00002B2B3636"
-FG_COLOR="#9999997B8383"
+## blatantly stolen from monokai
+palette="#494948483E3E:#F9F926267272:#A6A6E2E22E2E:#FDFD97971F1F:#6666D9D9EFEF:#AEAE8181FFFF:#A1A1EFEFE4E4:#F8F8F8F8F2F2:#757571715E5E:#FCFC5C5C9494:#C1C1F1F16161:#FEFEB2B25757:#8D8DE6E6F7F7:#C2C2A1A1FFFF:#BBBBF7F7EFEF:#F8F8F8F8F0F0"
+bd_color="#F8F8F8F8F2F2"
+fg_color="#F8F8F8F8F2F2"
+bg_color="#272728282222"
 
-gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ background-color $BG_COLOR
-gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ foreground-color $FG_COLOR
-gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ palette $PALETTE
-
+gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ background-color $bg_color
+gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ foreground-color $fg_color
+gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ bold-color $bd_color
+gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ palette $palette
 gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ use-theme-colors false
 gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ bold-color-same-as-fg false
+gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ font "Terminus 9"
+gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ cursor-shape ibeam
 
