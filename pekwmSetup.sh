e951a423756817030135246fa7eff16acf77042e
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2014-12-31 09:46:47 -0800

    remove indent

--- pekwmSetup.sh
+++ pekwmSetup.sh
@@ -3,6 +3,6 @@
 
 if [ -z "$( cat ~/.pekwm/keys | grep uxterm )" ]; then
   sed -i 's@KeyPress = "Mod4 E" { Actions = "Exec $TERM" }@KeyPress = "Mod4 E" { Actions = "Exec $TERM" } \
-    KeyPress = "Mod4 J" { Actions = "Exec uxterm" }@g' ~/.pekwm/keys
+  KeyPress = "Mod4 J" { Actions = "Exec uxterm" }@g' ~/.pekwm/keys
 fi
 
