4505db50e9b5638c870c675ce51be3c309b13f1b
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2014-12-31 09:45:48 -0800

    add a check

--- pekwmSetup.sh
+++ pekwmSetup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 ## just need a terminal shortcut!
 
-sed -i 's@KeyPress = "Mod4 E" { Actions = "Exec $TERM" }@KeyPress = "Mod4 E" { Actions = "Exec $TERM" } \
-  KeyPress = "Mod4 J" { Actions = "Exec uxterm" }@g' ~/.pekwm/keys
+if [ -z "$( cat ~/.pekwm/keys | grep uxterm )" ]; then
+  sed -i 's@KeyPress = "Mod4 E" { Actions = "Exec $TERM" }@KeyPress = "Mod4 E" { Actions = "Exec $TERM" } \
+    KeyPress = "Mod4 J" { Actions = "Exec uxterm" }@g' ~/.pekwm/keys
+fi
 
