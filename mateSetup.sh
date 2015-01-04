00dd236623a01dbd2bf28d4f9ccf9e6b86883c58
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-01-04 14:10:03 -0800

    i like CAPS for variables

--- mateSetup.sh
+++ mateSetup.sh
@@ -27,15 +27,15 @@ gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ use
 gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ scrollback-unlimited true
 
 ## blatantly stolen from monokai
-palette="#494948483E3E:#F9F926267272:#A6A6E2E22E2E:#FDFD97971F1F:#6666D9D9EFEF:#AEAE8181FFFF:#A1A1EFEFE4E4:#F8F8F8F8F2F2:#757571715E5E:#FCFC5C5C9494:#C1C1F1F16161:#FEFEB2B25757:#8D8DE6E6F7F7:#C2C2A1A1FFFF:#BBBBF7F7EFEF:#F8F8F8F8F0F0"
-bd_color="#F8F8F8F8F2F2"
-fg_color="#F8F8F8F8F2F2"
-bg_color="#272728282222"
+PALETTE="#494948483E3E:#F9F926267272:#A6A6E2E22E2E:#FDFD97971F1F:#6666D9D9EFEF:#AEAE8181FFFF:#A1A1EFEFE4E4:#F8F8F8F8F2F2:#757571715E5E:#FCFC5C5C9494:#C1C1F1F16161:#FEFEB2B25757:#8D8DE6E6F7F7:#C2C2A1A1FFFF:#BBBBF7F7EFEF:#F8F8F8F8F0F0"
+BD_COLOR="#F8F8F8F8F2F2"
+FG_COLOR="#F8F8F8F8F2F2"
+BG_COLOR="#272728282222"
 
-gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ background-color $bg_color
-gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ foreground-color $fg_color
-gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ bold-color $bd_color
-gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ palette $palette
+gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ background-color $BG_COLOR
+gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ foreground-color $FG_COLOR
+gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ bold-color $BD_COLOR
+gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ palette $PALETTE
 gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ use-theme-colors false
 gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ bold-color-same-as-fg false
 gsettings set org.mate.terminal.profile:/org/mate/terminal/profiles/default/ cursor-shape ibeam
