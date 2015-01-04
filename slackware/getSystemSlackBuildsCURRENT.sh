87175e5786512a130ddcc8776aef8b95a5bbe25d
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-01-03 21:11:45 -0800

    whitespace purge

--- slackware/getSystemSlackBuildsCURRENT.sh
+++ slackware/getSystemSlackBuildsCURRENT.sh
@@ -3,7 +3,7 @@
 ## downloads the source directory
 ## useful for updating stock packages
 
-if [ "$( uname -m )" = "x86_64" ]; then
+if [ "$(uname -m)" = "x86_64" ]; then
   rsync -avz rsync://slackware.cs.utah.edu/slackware/slackware64-current/source/ ~/source/
 else
   rsync -avz rsync://slackware.cs.utah.edu/slackware/slackware-current/source/ ~/source/
