4889ed716873053943b97f0eb039b6f46c2d3390
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-01-03 21:10:38 -0800

    whitespace purge

--- slackware/getExtraSlackBuilds.sh
+++ slackware/getExtraSlackBuilds.sh
@@ -3,7 +3,7 @@
 ## downloads the extra directory, containing additional packages
 ## and SlackBuilds not in the main Slackware tree
 
-if [ "$( uname -m )" = "x86_64" ]; then
+if [ "$(uname -m)" = "x86_64" ]; then
   rsync -avz rsync://slackware.cs.utah.edu/slackware/slackware64-current/extra/ ~/extra/
 else
   rsync -avz rsync://slackware.cs.utah.edu/slackware/slackware-current/extra/ ~/extra/
