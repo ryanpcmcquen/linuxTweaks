c3263b0cb16981fbf65130edf84bdb6151f14abe
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-01-03 21:10:57 -0800

    whitespace purge

--- slackware/getJavaSlackBuild.sh
+++ slackware/getJavaSlackBuild.sh
@@ -2,7 +2,7 @@
 
 ## download Pat's awesome java SlackBuild
 
-if [ "$( uname -m )" = "x86_64" ]; then
+if [ "$(uname -m)" = "x86_64" ]; then
   rsync -avz rsync://slackware.cs.utah.edu/slackware/slackware64-current/extra/java/ ~/java/
 else
   rsync -avz rsync://slackware.cs.utah.edu/slackware/slackware-current/extra/java/ ~/java/
