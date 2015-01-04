605931bc6a33bbaf32d5737467554f8c677fc098
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-01-03 21:07:51 -0800

    whitespace purge

--- setMAKEFLAGS.sh
+++ setMAKEFLAGS.sh
@@ -2,36 +2,25 @@
 ## add this in /etc/profile to make it permanent  ;^)
 
 
-if [ -z "$( cat /etc/profile | grep 'MAKEFLAGS' )" ]; then
+## make compiling faster  ;-)
+if [ -z "$(cat /etc/profile | grep 'MAKEFLAGS')" ]; then
   echo >> /etc/profile
-  echo 'if (( $( nproc ) > 2 )); then' >> /etc/profile
+  echo 'if (( $(nproc) > 2 )); then' >> /etc/profile
   ## cores--
-  echo '  export MAKEFLAGS="-j$( expr $( nproc ) - 1 )"' >> /etc/profile
+  echo '  export MAKEFLAGS=" -j$(expr $(nproc) - 1) "' >> /etc/profile
   ## half the cores
-  #echo '  export MAKEFLAGS="-j$( expr $( nproc ) / 2 )"' >> /etc/profile
+  #echo '  export MAKEFLAGS=" -j$(expr $(nproc) / 2) "' >> /etc/profile
   echo 'else' >> /etc/profile
-  echo '  export MAKEFLAGS="-j1"' >> /etc/profile
+  echo '  export MAKEFLAGS=" -j1 "' >> /etc/profile
   echo 'fi' >> /etc/profile
   echo >> /etc/profile
 fi
 
+## otherwise all our new stuff won't load until we log in again  ;^)
+. /etc/profile
 
-## awesome way
-#if (( $( nproc ) > 2 )); then
-#  export MAKEFLAGS="-j$( expr $( nproc ) - 1 )"
-#else
-#  export MAKEFLAGS="-j1"
-#fi
-#echo $MAKEFLAGS
-
-
-## semi-awesome way
-#if (( $( grep -c ^processor /proc/cpuinfo ) > 2 )); then
-#  export MAKEFLAGS="-j$( expr $( grep -c ^processor /proc/cpuinfo ) - 1 )"
-#else
-#  export MAKEFLAGS="-j1"
-#fi
-
-## in case you need to check your work
-#echo $MAKEFLAGS
-
+if (( $(nproc) > 2 )); then
+  export MAKEFLAGS=" -j$(expr $(nproc) - 1) "
+else
+  export MAKEFLAGS=" -j1 "
+fi
