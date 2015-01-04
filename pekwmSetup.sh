56c47229c13225d440565170155da46249c638ff
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-01-03 21:09:24 -0800

    whitespace purge

--- pekwmSetup.sh
+++ pekwmSetup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ## just need a terminal shortcut!
 
-if [ -z "$( cat ~/.pekwm/keys | grep uxterm )" ]; then
+if [ -z "$(cat ~/.pekwm/keys | grep uxterm)" ]; then
   sed -i 's@KeyPress = "Mod4 E" { Actions = "Exec $TERM" }@KeyPress = "Mod4 E" { Actions = "Exec $TERM" } \
   KeyPress = "Mod4 J" { Actions = "Exec uxterm" }@g' ~/.pekwm/keys
 fi
