e4e020c5e187f1bf1e48e2394523b7f03039e0bb
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-01-04 20:48:44 -0800

    just need multi file support now

--- slackware/imgult-server/imgult.php
+++ slackware/imgult-server/imgult.php
@@ -2,10 +2,12 @@
 $result=0;
 if ($result==0) echo shell_exec("rm -rf uploads/ 2>&1 && mkdir uploads/ 2>&1");
 if (trim($_POST["action"]) == "IMGULT!") {
-  $target_dir = "uploads/";
-  $imagename = $target_dir . basename($_FILES['image_file']['name']);
-  $result = @move_uploaded_file($_FILES['image_file']['tmp_name'], $imagename);
-  if ($result==1) echo("Successfully uploaded: <b>".$imagename."</b>");
+  for ($i=0; $i<count($_FILES['image_file']['name']); $i++) {
+    $target_dir = "uploads/";
+    $imagename = $target_dir . basename($_FILES['image_file']['name']);
+    $result = @move_uploaded_file($_FILES['image_file']['tmp_name'], $imagename);
+    if ($result==1) echo("Successfully uploaded: <b>".$imagename."</b>");
+  }
 }
 ?>
 <html>
@@ -30,12 +32,12 @@ input[type="file"] {
 input[type="submit"].imgulter {
   background-color: #046631;
   font-size: 2rem;
-  box-shadow: 0.3rem 0.3rem 0.7rem hsla(0, 0%, 0%, 0.3);
   color: #eeeeee;
+  margin-bottom: 1rem;
 }
 h1 {
   text-align: center;
-  text-shadow: 0.1rem 0.1rem 0.4rem hsla(0, 0%, 0%, 0.6);
+  text-shadow: 0.1rem 0.1rem 0.2rem hsla(0, 0%, 0%, 0.6);
 }
 </style>
 <script>
@@ -47,7 +49,7 @@ function clearForm() {
 <body>
 <h1>imgult for web</h1>
 <form method='POST' enctype='multipart/form-data' name='frmmain' action='imgult.php'>
-<input type="file" id="image_dump" name="image_file" multiple>
+<input type="file" id="image_dump" name="image_file" multiple="true">
 <br>
 <input class="imgulter" type="submit" value="  IMGULT!  " name="action">
 <input type="submit" value="reset" name="action" onClick="clearForm();">
