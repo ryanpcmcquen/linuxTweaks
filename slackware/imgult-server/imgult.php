3db40a931df4a4557fa5da9f59f11ed019579a5d
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-01-04 20:22:06 -0800

    works with uploads/ folder

--- slackware/imgult-server/imgult.php
+++ slackware/imgult-server/imgult.php
@@ -1,11 +1,12 @@
 <?php
 $result=0;
-if (trim($_POST["action"]) == "upload") { //**** User Clicked the Upload File Button
-   //*********** Execute the Following Code to Upload File *************
-   $imagename = basename($_FILES['image_file']['name']);  // grab name of file 
-   $result = @move_uploaded_file($_FILES['image_file']['tmp_name'], $imagename); // upload it 
-   if ($result==1) echo("Successfully uploaded: <b>".$imagename."</b>"); // did it work?
-} // end if
+if ($result==0) echo shell_exec("rm -rf uploads/ 2>&1 && mkdir uploads/ 2>&1");
+if (trim($_POST["action"]) == "IMGULT!") {
+  $target_dir = "uploads/";
+  $imagename = $target_dir . basename($_FILES['image_file']['name']);
+  $result = @move_uploaded_file($_FILES['image_file']['tmp_name'], $imagename);
+  if ($result==1) echo("Successfully uploaded: <b>".$imagename."</b>");
+}
 ?>
 <html>
 <head>
@@ -18,18 +19,34 @@ input {
   justify-content: center;
   margin: 0 auto;
   text-align: center;
+  box-shadow: 2px 2px 5px hsla(0, 0%, 0%, 0.3);
+  font-family: sans-serif;
+  font-size: 1rem;
+  border-radius: 20px;
+}
+input[type="file"] {
+  background-color: hsla(360, 100%, 100%, 0.8);
+  color: #000000;
+}
+input[type="submit"] {
+  background-color: #046631;
+  color: #eeeeee;
+}
+h1 {
+  text-align: center;
 }
 </style>
 </head>
 <body>
+<h1>imgult for web</h1>
 <form method='POST' enctype='multipart/form-data' name='frmmain' action='imgult.php'>
-<input type="file" multiple name="image_file">
+<input type="file" name="image_file" multiple>
 <br>
-<input type="submit" value="  upload  " name="action">
+<input type="submit" value="  IMGULT!  " name="action">
 </form>
 <br>
 <?php
-if ($result==1) echo shell_exec("./imgult ".$imagename." 2>&1");
+if ($result==1) echo shell_exec("./imgult 2>&1");
 if ($result==1) echo("<img src='".$imagename."'>"); // display the uploaded file
 ?>
 </body>
