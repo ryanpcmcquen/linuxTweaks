6b0d371da2a6b43afc0140bda6cc25aa61929416
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-01-04 19:37:06 -0800

    working version, close to multiple uploads

--- slackware/imgult-server/imgult.php
+++ slackware/imgult-server/imgult.php
@@ -1,22 +1,31 @@
 <?php
 $result=0;
-if (trim($_POST["action"]) == "Upload File") { //**** User Clicked the Upload File Button
+if (trim($_POST["action"]) == "upload") { //**** User Clicked the Upload File Button
    //*********** Execute the Following Code to Upload File *************
    $imagename = basename($_FILES['image_file']['name']);  // grab name of file 
    $result = @move_uploaded_file($_FILES['image_file']['tmp_name'], $imagename); // upload it 
    if ($result==1) echo("Successfully uploaded: <b>".$imagename."</b>"); // did it work?
 } // end if
-?>          
+?>
 <html>
 <head>
-<title>Upload file script</title>
+<title>imgult for web</title>
+<style>
+input {
+  padding: 5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 auto;
+  text-align: center;
+}
+</style>
 </head>
 <body>
-
 <form method='POST' enctype='multipart/form-data' name='frmmain' action='imgult.php'>
-Image: <input type="file" name="image_file">
+<input type="file" multiple name="image_file">
 <br>
-<input type="submit" value="  Upload File  " name="action">
+<input type="submit" value="  upload  " name="action">
 </form>
 <br>
 <?php
