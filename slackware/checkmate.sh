8c139eaa6c36e836239762b7b1bf5cce2c8d2db1
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-02-07 11:29:20 -0800

    add testing var (not default)

--- slackware/checkmate.sh
+++ slackware/checkmate.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 ## wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/slackware/checkmate.sh -P ~/msb/
 
+TESTING=${TESTING:-no}
+
 ## blacklist all msb packages so they are not overwritten/removed
 if [ -z "$(cat /etc/slackpkg/blacklist | grep [0-9]+_msb)" ]; then
   echo [0-9]+_msb >> /etc/slackpkg/blacklist
@@ -10,8 +12,10 @@ fi
 sh mate-build-deps.sh
 sh mate-build-base.sh
 sh mate-build-extra.sh
-cd testing
-sh mate-build-testing.sh
 
-cd
+if [ "$TESTING" = "yes" ]; then
+  cd testing
+  sh mate-build-testing.sh
+  cd
+fi
 
