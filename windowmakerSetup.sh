5da4885b0a0d1d268d9c545138e899127bf8555f
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-02-08 19:40:41 -0800

    add close & run shortcuts

--- windowmakerSetup.sh
+++ windowmakerSetup.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
+## curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/windowmakerSetup.sh | sh
+
+## everyone loves the terminal, says the unix nerd
 if [ -z "$(cat ~/GNUstep/Defaults/WMRootMenu | grep 'Mod1+F1')" ]; then
-  sed -i 's@\(Konsole\,\ EXEC\,\ konsole\)@\
-  \(Konsole, EXEC, konsole),\
+  sed -i 's@(Konsole, EXEC, konsole)@\
+  (Konsole, EXEC, konsole),\
   (uxterm, SHORTCUT, "Mod1+F1", EXEC, "uxterm")\
   @g' ~/GNUstep/Defaults/WMRootMenu
 fi
+
+## kind of doing a fluxbox thing here
+if [ -z "$(cat ~/GNUstep/Defaults/WMRootMenu | grep 'Mod1+F2')" ]; then
+  sed -i 's@(Run..., SHEXEC, "%A(Run, Type command:)"),@\
+  (Run..., SHEXEC, "%A(Run, Type command:)"),\
+  (Run..., SHORTCUT, "Mod1+F2", EXEC, "%A(Run, Type command:)"),\
+  @g' ~/GNUstep/Defaults/WMRootMenu
+fi
+
+## typical alt + f4 close key
+sed -i 's/CloseKey = None;/CloseKey = "Mod1+F4";/g' ~/GNUstep/Defaults/WindowMaker
+
