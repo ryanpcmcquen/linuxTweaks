2afa7b8e47369963c9be3e790672c606ae338113
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-02-09 07:20:31 -0800

    cruft clean, and use find instead of ls

--- slackware/switchToGenericKernel.sh
+++ slackware/switchToGenericKernel.sh
@@ -9,11 +9,7 @@ if [ -e ~/liloGenericEntry.sh ]; then
   cp ~/liloGenericEntry.sh ~/liloGenericEntry.sh.bak
 fi
 
-### works, but inefficient
-##ls --color=never /boot/vmlinuz-generic-* | tail -1 > ~/liloGenericEntry.sh
-##echo "/usr/share/mkinitrd/mkinitrd_command_generator.sh -l $(cat liloGenericEntry.sh)" > ~/liloGenericEntry.sh
-
-echo "/usr/share/mkinitrd/mkinitrd_command_generator.sh -l $(ls --color=never /boot/vmlinuz-generic-* | tail -1)" > ~/liloGenericEntry.sh
+echo "/usr/share/mkinitrd/mkinitrd_command_generator.sh -l $(find /boot/ -name vmlinuz-generic-* | tail -1)" > ~/liloGenericEntry.sh
 
 sh ~/liloGenericEntry.sh >> /etc/lilo.conf
 
