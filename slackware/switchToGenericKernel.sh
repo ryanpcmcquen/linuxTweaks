fddf45c196d79daa7022039d9ef9c4c58fa1a121
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-02-09 07:36:25 -0800

    sometimes you gotta quote

--- slackware/switchToGenericKernel.sh
+++ slackware/switchToGenericKernel.sh
@@ -9,7 +9,7 @@ if [ -e ~/liloGenericEntry.sh ]; then
   cp ~/liloGenericEntry.sh ~/liloGenericEntry.sh.bak
 fi
 
-echo "/usr/share/mkinitrd/mkinitrd_command_generator.sh -l $(find /boot/ -name vmlinuz-generic-* | tail -1)" > ~/liloGenericEntry.sh
+echo "/usr/share/mkinitrd/mkinitrd_command_generator.sh -l $(find /boot/ -name 'vmlinuz-generic-*' | tail -1)" > ~/liloGenericEntry.sh
 
 sh ~/liloGenericEntry.sh >> /etc/lilo.conf
 
