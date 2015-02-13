da2ce7f153a5e6f1f9d75d9ad0b8909f04e9a1d9
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-02-13 13:31:07 -0800

    fix lilo entry command

--- slackware/switchToGenericKernel.sh
+++ slackware/switchToGenericKernel.sh
@@ -12,7 +12,7 @@ fi
 echo "/usr/share/mkinitrd/mkinitrd_command_generator.sh -l $(find /boot/ -name 'vmlinuz-generic-*' | tail -1)" > ~/liloGenericEntry.sh
 
 ## check for duplicate entries
-if [ -z "grep '$(cat ~/liloGenericEntry.sh | cut -d- -f4)' /etc/lilo.conf" ]; then
+if [ -z "`grep $(cat ~/liloGenericEntry.sh | cut -d- -f4) /etc/lilo.conf`" ]; then
   sh ~/liloGenericEntry.sh >> /etc/lilo.conf
 fi
 
