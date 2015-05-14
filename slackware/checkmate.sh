7ca6d270a82a6760b84384c56b53d95a039a64fc
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-05-14 07:05:29 -0700

    add gtk detector

--- slackware/checkmate.sh
+++ slackware/checkmate.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 ## wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/slackware/checkmate.sh -P ~/msb/
 
+## run with TESTING=yes for testing pkgs
 TESTING=${TESTING:-no}
+## detect gtk version for mate-themes package
+export GTKVERSION=${GTKVERSION:-gtk`find /var/log/packages/ -name gtk\+3* | cut -d/ -f5 | cut -d- -f2 | cut -d. -f1-2`}
 
 ## blacklist all msb packages so they are not overwritten/removed
 if [ -z "$(cat /etc/slackpkg/blacklist | grep _msb)" ]; then
