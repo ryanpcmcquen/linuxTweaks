6dafb5f40da1352d9e8385e68eeafaadca165aba
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-06-20 08:55:02 -0700

    add git and vim settings

--- ubuntu/ubuntu1404Setup.sh
+++ ubuntu/ubuntu1404Setup.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-ESSENTIALPKGS="vim vim-gtk build-essential zlib1g-dev libsndfile1-dev libsdl1.2-dev"
+ESSENTIALPKGS="git vim vim-gtk build-essential zlib1g-dev libsndfile1-dev libsdl1.2-dev"
 
 sudo apt-get install $ESSENTIALPKGS
+
+## retrieve git and vim settings
+curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/gitVimNORMALorROOT.sh | sh
