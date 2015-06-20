0b64cd23309780623752320fe6c5e28e9f379a93
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-06-19 19:05:57 -0700

    Create fedoraSetup.sh

---
+++ fedora/fedoraSetup.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+## run as a normal user
+
+## variables for gsettings
+KEYS_SCHEMA=org.gnome.settings-daemon.plugins.media-keys
+CUSTOM_SCHEMA=${KEYS_SCHEMA}.custom-keybinding
+CUSTOM_PATH=/${CUSTOM_SCHEMA//.//}s/custom0/
+## set the terminal shortcut to ctrl+alt+t
+gsettings set $CUSTOM_SCHEMA:$CUSTOM_PATH name 'Terminal'
+gsettings set $CUSTOM_SCHEMA:$CUSTOM_PATH command 'gnome-terminal'
+gsettings set $CUSTOM_SCHEMA:$CUSTOM_PATH binding '<Control><Alt>t'
+gsettings set $KEYS_SCHEMA custom-keybindings "[\"$CUSTOM_PATH\"]"
+
+## pretty sure the first 3 are included, but it can't hurt
+ESSENTIALPKGS="curl wget make vim vim-X11 gcc gcc-c++ clang kernel-headers kernel-devel zlib-devel libsndfile-devel SDL-devel"
+
+## if dnf fails, the system is probably older
+sudo dnf -y install $ESSENTIALPKGS || sudo yum -y install $ESSENTIALPKGS
+
+## google chrome
+if [ "`uname -m`" = "x86_64" ]; then
+  wget -N https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm -P ~/Downloads/
+  sudo dnf -y install ~/Downloads/google-chrome-stable_current_x86_64.rpm \
+    || sudo yum -y install ~/Downloads/google-chrome-stable_current_x86_64.rpm
+else
+  wget -N https://dl.google.com/linux/direct/google-chrome-stable_current_i386.rpm -P ~/Downloads/
+  sudo dnf -y install ~/Downloads/google-chrome-stable_current_i386.rpm \
+    || sudo yum -y install ~/Downloads/google-chrome-stable_current_i386.rpm
+fi
+
