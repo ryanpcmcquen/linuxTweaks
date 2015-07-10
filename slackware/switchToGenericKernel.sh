f052f758d71d67e54a1542209209e98f716ede23
Ryan P.C. McQuen <ryan.q@linux.com>
2015-07-10 07:26:42 -0700

    sort necessary find commands
    Signed-off-by: Ryan P.C. McQuen <ryan.q@linux.com>

--- slackware/switchToGenericKernel.sh
+++ slackware/switchToGenericKernel.sh
@@ -11,7 +11,7 @@ if [ -e /etc/lilo.conf ]; then
     rm -v ~/liloGenericEntry.sh
   fi
   ## this is the magic
-  echo "/usr/share/mkinitrd/mkinitrd_command_generator.sh -l $(find /boot/ -name 'vmlinuz-generic-*' | tail -1)" > ~/liloGenericEntry.sh
+  echo "/usr/share/mkinitrd/mkinitrd_command_generator.sh -l $(find /boot/ -name 'vmlinuz-generic-*' | sort | tail -1)" > ~/liloGenericEntry.sh
   ## check for duplicate entries
   if [ -z "`grep $(cat ~/liloGenericEntry.sh | cut -d- -f4) /etc/lilo.conf`" ]; then
     sh ~/liloGenericEntry.sh >> /etc/lilo.conf
