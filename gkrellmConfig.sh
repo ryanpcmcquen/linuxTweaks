9d27c52c5baae768a2cd803068a2c12692231f0f
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-07-14 19:06:56 -0700

    make backups!

--- gkrellmConfig.sh
+++ gkrellmConfig.sh
@@ -4,15 +4,15 @@
 
 #wget -N http://www.muhri.net/gkrellm/GKrellM-Skins.tar.gz -P ~/
 
-sed -i 's/enable_sysname 0/enable_sysname 1/g' ~/.gkrellm2/user-config
+sed -i.bak 's/enable_sysname 0/enable_sysname 1/g' ~/.gkrellm2/user-config
 
-sed -i 's/skip_taskbar 0/skip_taskbar 1/g' ~/.gkrellm2/user-config
-sed -i 's/skip_pager 0/skip_pager 1/g' ~/.gkrellm2/user-config
+sed -i.bak 's/skip_taskbar 0/skip_taskbar 1/g' ~/.gkrellm2/user-config
+sed -i.bak 's/skip_pager 0/skip_pager 1/g' ~/.gkrellm2/user-config
 
-sed -i 's/save_position 0/save_position 1/g' ~/.gkrellm2/user-config
+sed -i.bak 's/save_position 0/save_position 1/g' ~/.gkrellm2/user-config
 
-sed -i 's/battery enable 0/battery enable 1/g' ~/.gkrellm2/user-config
+sed -i.bak 's/battery enable 0/battery enable 1/g' ~/.gkrellm2/user-config
 
-sed -i 's/mail enable 1 0 0 0/mail enable 0 0 0 0/g' ~/.gkrellm2/user-config
+sed -i.bak 's/mail enable 1 0 0 0/mail enable 0 0 0 0/g' ~/.gkrellm2/user-config
 
 
