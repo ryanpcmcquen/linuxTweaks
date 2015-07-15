009764c30b9be8ab0b34bbcc0945ac45220fb503
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-07-14 19:09:29 -0700

    make backups!

--- windowmakerSetup.sh
+++ windowmakerSetup.sh
@@ -4,7 +4,7 @@
 
 ## everyone loves the terminal, says the unix nerd
 if [ -z "$(cat ~/GNUstep/Defaults/WMRootMenu | grep 'Mod1+F1')" ]; then
-  sed -i 's@(Konsole, EXEC, konsole)@\
+  sed -i.bak 's@(Konsole, EXEC, konsole)@\
   (Konsole, EXEC, konsole),\
   (uxterm, SHORTCUT, "Mod1+F1", EXEC, "uxterm")\
   @g' ~/GNUstep/Defaults/WMRootMenu
@@ -12,15 +12,15 @@ fi
 
 ## kind of doing a fluxbox thing here
 if [ -z "$(cat ~/GNUstep/Defaults/WMRootMenu | grep 'Mod1+F2')" ]; then
-  sed -i 's@(Run..., SHEXEC, "%A(Run, Type command:)"),@\
+  sed -i.bak 's@(Run..., SHEXEC, "%A(Run, Type command:)"),@\
   (Run..., SHEXEC, "%A(Run, Type command:)"),\
   (Run..., SHORTCUT, "Mod1+F2", EXEC, "%A(Run, Type command:)"),\
   @g' ~/GNUstep/Defaults/WMRootMenu
 fi
 
 ## typical alt + f4 close key
-sed -i 's/CloseKey = None;/CloseKey = "Mod1+F4";/g' ~/GNUstep/Defaults/WindowMaker
+sed -i.bak 's/CloseKey = None;/CloseKey = "Mod1+F4";/g' ~/GNUstep/Defaults/WindowMaker
 
 ## alt+m is minimize, may as well make alt+n maximize
-sed -i 's/MaximizeKey = None;/MaximizeKey = "Mod1+N";/g' ~/GNUstep/Defaults/WindowMaker
+sed -i.bak 's/MaximizeKey = None;/MaximizeKey = "Mod1+N";/g' ~/GNUstep/Defaults/WindowMaker
 
