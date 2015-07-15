260938b27e80bd4b8e941668d9e7cb1dfdc9f278
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-07-14 19:07:20 -0700

    make backups!

--- luminaSetup.sh
+++ luminaSetup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 if [ -z "$(cat ~/.lumina/fluxbox-init | grep zimek_darkblue)" ]; then
-  sed -i "s@/usr/share/fluxbox/styles/.*@/usr/share/fluxbox/styles/zimek_darkblue@g" ~/.lumina/fluxbox-init
+  sed -i.bak "s@/usr/share/fluxbox/styles/.*@/usr/share/fluxbox/styles/zimek_darkblue@g" ~/.lumina/fluxbox-init
 fi
 
