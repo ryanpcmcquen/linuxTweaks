33b940d3be59f6c510cf0b1f2f9228c13fdbb0db
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-08-18 07:21:08 -0700

    add key repeat/delay settings (for speed)

--- fedora/fedoraSetup.sh
+++ fedora/fedoraSetup.sh
@@ -12,6 +12,11 @@ gsettings set $CUSTOM_SCHEMA:$CUSTOM_PATH command 'gnome-terminal'
 gsettings set $CUSTOM_SCHEMA:$CUSTOM_PATH binding '<Control><Alt>t'
 gsettings set $KEYS_SCHEMA custom-keybindings "[\"$CUSTOM_PATH\"]"
 
+## make the keyboard crazy fast
+gsettings set org.gnome.settings-daemon.peripherals.keyboard repeat true
+gsettings set org.gnome.settings-daemon.peripherals.keyboard delay uint32 150
+gsettings set org.gnome.settings-daemon.peripherals.keyboard repeat-interval uint32 80
+
 ## retrieve git and vim settings
 curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/gitVimNORMALorROOT.sh | sh
 
