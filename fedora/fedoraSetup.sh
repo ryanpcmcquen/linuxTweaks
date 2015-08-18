895c3bb078a7bfc142040f3c741ee1f3251ddb25
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-08-18 11:34:17 -0700

    add touchpad settings, just in case

--- fedora/fedoraSetup.sh
+++ fedora/fedoraSetup.sh
@@ -16,6 +16,10 @@ gsettings set $KEYS_SCHEMA custom-keybindings "[\"$CUSTOM_PATH\"]"
 gsettings set org.gnome.settings-daemon.peripherals.keyboard repeat true
 gsettings set org.gnome.settings-daemon.peripherals.keyboard delay uint32 150
 gsettings set org.gnome.settings-daemon.peripherals.keyboard repeat-interval uint32 80
+## sensible touchpad defaults [these may already be set, but it cannot hurt]
+gsettings set org.gnome.settings-daemon.peripherals.touchpad scroll-method 2
+gsettings set org.gnome.settings-daemon.peripherals.touchpad horiz-scroll-enabled true
+gsettings set org.gnome.settings-daemon.peripherals.touchpad tap-to-click false
 
 ## retrieve git and vim settings
 curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/gitVimNORMALorROOT.sh | sh
