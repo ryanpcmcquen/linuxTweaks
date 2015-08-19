78e68945ec6241bdeb4bf9f7a063bf2237465bd9
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-08-19 07:15:09 -0700

    i am all about the subtle differences

--- fedora/fedoraSetup.sh
+++ fedora/fedoraSetup.sh
@@ -22,7 +22,7 @@ gsettings set org.gnome.desktop.peripherals.keyboard repeat true
 gsettings set org.gnome.desktop.peripherals.keyboard delay 150
 gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 80
 ## sensible touchpad defaults [these may already be set, but it cannot hurt]
-gsettings set org.gnome.desktop.peripherals.touchpad scroll-method 2
+gsettings set org.gnome.desktop.peripherals.touchpad scroll-method 'two-finger-scrolling'
 ## apparently this key was removed at some point
 #gsettings set org.gnome.desktop.peripherals.touchpad horiz-scroll-enabled true
 gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click false
