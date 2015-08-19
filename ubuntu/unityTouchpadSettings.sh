19039acea3de00079c4666c2d8065f27f0189529
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-08-19 16:56:00 -0700

    fix these to be valid

--- ubuntu/unityTouchpadSettings.sh
+++ ubuntu/unityTouchpadSettings.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 ## this used to use dconf, which is a bad idea
-gsettings set org.mate.peripherals-touchpad scroll-method 2
-gsettings set org.mate.peripherals-touchpad horiz-scroll-enabled true
-gsettings set org.mate.peripherals-touchpad tap-to-click false
+gsettings set org.gnome.settings-daemon.peripherals.touchpad scroll-method 'two-finger-scrolling'
+gsettings set org.gnome.settings-daemon.peripherals.touchpad horiz-scroll-enabled true
+gsettings set org.gnome.settings-daemon.peripherals.touchpad tap-to-click false
