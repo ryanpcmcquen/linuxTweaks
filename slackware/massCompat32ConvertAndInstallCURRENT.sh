1b5c662b3c37e155620ef7d030537cc0b5fd2367
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-11-13 10:27:53 -0800

    Create massCompat32ConvertAndInstallCURRENT.sh

---
+++ slackware/massCompat32ConvertAndInstallCURRENT.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+##
+## runs the appropriate mass conversion and blacklists all the subsequent goodies
+##
+## curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/slackware/massCompat32ConvertAndInstall.sh | sh
+
+massconvert32.sh -u http://slackware.cs.utah.edu/pub/slackware/slackware-current/slackware/
+
+if [ -z "$(cat /etc/slackpkg/blacklist | grep _compat32)" ]; then
+  echo "[0-9]+compat32" >> /etc/slackpkg/blacklist
+fi
