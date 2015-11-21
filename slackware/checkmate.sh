9e7a1397607d5bfe2dfa455f1b090d3a037c50f7
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-11-21 12:23:47 -0800

    add extra flags; make extra not build be default

--- slackware/checkmate.sh
+++ slackware/checkmate.sh
@@ -3,6 +3,10 @@
 
 ## run with TESTING=yes for testing pkgs
 TESTING=${TESTING:-no}
+## run with EXTRA=yes for extra pkgs
+EXTRA=${EXTRA:-no}
+## build TESTING and EXTRA by using BUILDALL=yes
+BUILDALL=${BUILDALL:-no}
 ## detect gtk version for mate-themes package
 export GTKVERSION=${GTKVERSION:-gtk`find /var/log/packages/ -name gtk\+3* | sort | tail -1 | cut -d/ -f5 | cut -d- -f2 | cut -d. -f1-2`}
 
@@ -14,9 +18,12 @@ fi
 
 sh mate-build-deps.sh
 sh mate-build-base.sh
-sh mate-build-extra.sh
 
-if [ "$TESTING" = "yes" ]; then
+if [ "$EXTRA" = "yes" ] || [ "$BUILDALL" = "yes" ]; then
+  sh mate-build-extra.sh
+fi
+
+if [ "$TESTING" = "yes" ] || [ "$BUILDALL" = "yes" ]; then
   cd testing
   sh mate-build-testing.sh
   cd
