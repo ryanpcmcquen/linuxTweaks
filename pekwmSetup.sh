ccbaff2a8d7fea9d3ac6b42192426cb25eabf238
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-11-22 08:26:49 -0800

    reduce UUOC

--- pekwmSetup.sh
+++ pekwmSetup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 ## just need a terminal shortcut!
 
-if [ -z "$(cat ~/.pekwm/keys | grep uxterm)" ]; then
+if [ -z "$(grep uxterm ~/.pekwm/keys)" ]; then
   sed -i.bak 's@KeyPress = "Mod4 E" { Actions = "Exec $TERM" }@KeyPress = "Mod4 E" { Actions = "Exec $TERM" } \
   KeyPress = "Mod4 J" { Actions = "Exec uxterm" }@g' ~/.pekwm/keys
 fi
