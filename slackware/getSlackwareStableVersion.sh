c231be77bd7a6c141b15a73b4f272eeb679e4b59
Ryan P.C. McQuen <ryanpcmcquen@gmail.com>
2015-11-22 08:27:24 -0800

    reduce UUOC

--- slackware/getSlackwareStableVersion.sh
+++ slackware/getSlackwareStableVersion.sh
@@ -3,7 +3,7 @@
 cd
 ## get stable slackware version
 wget www.slackware.com -O ~/slackware-home-page.html
-cat ~/slackware-home-page.html | grep "is released!" | head -1 | sed 's/Slackware//g' | \
+grep "is released!" ~/slackware-home-page.html | head -1 | sed 's/Slackware//g' | \
   sed 's/is released!//g' | sed 's/ //g' > ~/slackwareStableVersion
 rm -v ~/slackware-home-page.html
 
