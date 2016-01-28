1b06b9c02e5fdbd7e93529349bc1483f32cc3dae
Ryan P.C. McQuen <ryan.q@linux.com>
2016-01-28 07:53:27 -0800

    group panel stuff together

--- .xfceSetup.sh
+++ .xfceSetup.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-## curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/xfceSetup.sh | sh
+## curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.xfceSetup.sh | sh
 
 ## thanks to @bfanning
 ## only one panel!
@@ -12,6 +12,35 @@ xfconf-query -n -c xfce4-panel -p /panels/panel-0/length -t int -s 100
 xfconf-query -n -c xfce4-panel -p /panels/panel-0/size -t int -s 32
 xfconf-query -n -c xfce4-panel -p /panels/panel-0/position -t string -s "p=6;x=0;y=0"
 xfconf-query -n -c xfce4-panel -p /panels/panel-0/position-locked -t bool -s true
+
+## pretty much xfce defaults
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-1 -t string -s applicationsmenu
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-2 -t string -s tasklist
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-3 -t string -s separator
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-4 -t string -s pager
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-5 -t string -s clock
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-6 -t string -s systray
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-7 -t string -s pulseaudio
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-8 -t string -s actions
+
+## make the top panel beautiful
+## applications menu
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-1/show-button-title -t bool -s false
+## separator
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-3/style -t int -s 0
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-3/expand -t bool -s true
+## window/task list
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-2/show-labels -t bool -s false
+## workspace pager,
+## i call this the "Theodore Ts'o" layout
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-4/rows -t int -s 3
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-4/miniature-view -t bool -s true
+xfconf-query -n -c xfwm4 -p /general/workspace_count -t int -s 9
+## clock (fuzzy mode)
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-5/show-frame -t bool -s false
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-5/mode -t int -s 3
+## systray
+xfconf-query -n -c xfce4-panel -p /plugins/plugin-6/show-frame -t bool -s false
 
 ## make window management awesome
 xfconf-query -n -c xfwm4 -p /general/snap_to_border -t bool -s true
@@ -58,35 +87,6 @@ xfconf-query -n -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Shift><A
 xfconf-query -n -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Shift><Alt>Left" -t string -s "move_window_left_workspace_key"
 xfconf-query -n -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Shift><Alt>Right" -t string -s "move_window_right_workspace_key"
 xfconf-query -n -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Shift><Alt>Up" -t string -s "move_window_up_workspace_key"
-
-## pretty much xfce defaults
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-1 -t string -s applicationsmenu
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-2 -t string -s tasklist
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-3 -t string -s separator
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-4 -t string -s pager
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-5 -t string -s clock
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-6 -t string -s systray
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-7 -t string -s pulseaudio
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-8 -t string -s actions
-
-## make the top panel beautiful
-## applications menu
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-1/show-button-title -t bool -s false
-## separator
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-3/style -t int -s 0
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-3/expand -t bool -s true
-## window/task list
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-2/show-labels -t bool -s false
-## workspace pager,
-## i call this the "Theodore Ts'o" layout
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-4/rows -t int -s 3
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-4/miniature-view -t bool -s true
-xfconf-query -n -c xfwm4 -p /general/workspace_count -t int -s 9
-## clock (fuzzy mode)
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-5/show-frame -t bool -s false
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-5/mode -t int -s 3
-## systray
-xfconf-query -n -c xfce4-panel -p /plugins/plugin-6/show-frame -t bool -s false
 
 ## make the desktop tidy
 xfconf-query -n -c xfce4-desktop -p /desktop-icons/file-icons/show-filesystem -t bool -s false
