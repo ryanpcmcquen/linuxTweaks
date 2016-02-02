01230beda78366816ac5287d02c6a6032a96b34b
Ryan P.C. McQuen <ryan.q@linux.com>
2016-02-02 13:24:30 -0800

    link to gnome stuff

--- fedora/.fedoraSetup.sh
+++ fedora/.fedoraSetup.sh
@@ -5,26 +5,7 @@
 ## always good to start with an update
 sudo dnf -y upgrade || sudo yum -y update
 
-## thanks to @fmuellner
-## variables for gsettings
-KEYS_SCHEMA=org.gnome.settings-daemon.plugins.media-keys
-CUSTOM_SCHEMA=${KEYS_SCHEMA}.custom-keybinding
-CUSTOM_PATH=/${CUSTOM_SCHEMA//.//}s/custom0/
-## set the terminal shortcut to ctrl+alt+t
-gsettings set $CUSTOM_SCHEMA:$CUSTOM_PATH name 'Terminal'
-gsettings set $CUSTOM_SCHEMA:$CUSTOM_PATH command 'gnome-terminal'
-gsettings set $CUSTOM_SCHEMA:$CUSTOM_PATH binding '<Control><Alt>t'
-gsettings set $KEYS_SCHEMA custom-keybindings "[\"$CUSTOM_PATH\"]"
-
-## make the keyboard crazy fast
-gsettings set org.gnome.desktop.peripherals.keyboard repeat true
-gsettings set org.gnome.desktop.peripherals.keyboard delay 150
-gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 80
-## sensible touchpad defaults [these may already be set, but it cannot hurt]
-gsettings set org.gnome.desktop.peripherals.touchpad scroll-method 'two-finger-scrolling'
-## apparently this key was removed at some point
-#gsettings set org.gnome.desktop.peripherals.touchpad horiz-scroll-enabled true
-gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click false
+curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.gnome3Setup | bash
 
 ## retrieve git and vim settings
 curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/gitVimNORMALorROOT.sh | sh
