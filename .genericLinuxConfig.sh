12e73fd65bdf1b67ba466851d3d9a32b4c54627c
Ryan P.C. McQuen <ryan.q@linux.com>
2016-03-22 07:07:57 -0700

    just add to non-root bash config

--- .genericLinuxConfig.sh
+++ .genericLinuxConfig.sh
@@ -15,21 +15,6 @@ if [ ! -d ~/.ssh/ ]; then
   chmod 644 .ssh/known_hosts
 fi
 
-## make ssh less annoying
-if [ -z "$(grep 'SSH_AUTH_SOCK' /etc/profile)" ]; then
-  if [ -z "`which sudo`" ]; then
-    echo "You do not have sudo, you may run this as root:"
-    echo "##echo >> /etc/profile.d/START_SSH_AGENT; echo 'if [ -z "$SSH_AUTH_SOCK" ]; then' >> /etc/profile.d/START_SSH_AGENT; echo '  eval `ssh-agent`' >> /etc/profile.d/START_SSH_AGENT; echo '  ssh-add' >> /etc/profile.d/START_SSH_AGENT; echo 'fi' >> /etc/profile.d/START_SSH_AGENT; echo >> /etc/profile.d/START_SSH_AGENT"
-  elif [ ! -e /etc/profile.d/START_SSH_AGENT ]; then
-    sudo sh -c "echo >> /etc/profile.d/START_SSH_AGENT"
-    sudo sh -c "echo 'if [ -z \"\$SSH_AUTH_SOCK\" ]; then' >> /etc/profile.d/START_SSH_AGENT"
-    sudo sh -c "echo '  eval \`ssh-agent\`' >> /etc/profile.d/START_SSH_AGENT"
-    sudo sh -c "echo '  ssh-add' >> /etc/profile.d/START_SSH_AGENT"
-    sudo sh -c "echo 'fi' >> /etc/profile.d/START_SSH_AGENT"
-    sudo sh -c "echo >> /etc/profile.d/START_SSH_AGENT"
-  fi
-fi
-
 ## copy this version if you do not have sudo
 ##echo >> /etc/profile.d/START_SSH_AGENT; echo 'if [ -z "$SSH_AUTH_SOCK" ]; then' >> /etc/profile.d/START_SSH_AGENT; echo '  eval `ssh-agent`' >> /etc/profile.d/START_SSH_AGENT; echo '  ssh-add' >> /etc/profile.d/START_SSH_AGENT; echo 'fi' >> /etc/profile.d/START_SSH_AGENT; echo >> /etc/profile.d/START_SSH_AGENT
 
@@ -53,6 +38,15 @@ if [ -e $HOME/.bashrc ]; then
     BASHFILE=".bash_profile"
   else
     BASHFILE=".bashrc"
+  fi
+  ## make ssh less annoying
+  if [ -z "$(grep 'SSH_AUTH_SOCK' ${BASHFILE})" ]; then
+    echo >> ~/${BASHFILE}
+    echo 'if [ -z \"\$SSH_AUTH_SOCK\" ]; then' >> ~/${BASHFILE}
+    echo '  eval \`ssh-agent\`' >> ~/${BASHFILE}
+    echo '  ssh-add' >> ~/${BASHFILE}
+    echo 'fi' >> ~/${BASHFILE}
+    echo >> ~/${BASHFILE}
   fi
   if [ -z "`grep 'for DIR in' ${BASHFILE}`" ]; then
     echo >> ~/${BASHFILE}
