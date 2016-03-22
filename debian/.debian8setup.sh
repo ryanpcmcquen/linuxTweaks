848b66dd4afd5871a08496dec290025a729872d2
Ryan P.C. McQuen <ryan.q@linux.com>
2016-03-22 06:56:27 -0700

    use `wget` as `curl` isn't in a default install

--- debian/.debian8setup.sh
+++ debian/.debian8setup.sh
@@ -2,7 +2,10 @@
 ## wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/debian/.debian8setup.sh -P ~/; bash ~/.debian8setup.sh
 ## basically just gnome and git/vim stuff
 
-curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.gnome3Setup | bash
+wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.gnome3Setup -P ~/; bash ~/.gnome3Setup
 
 ## retrieve git and vim settings
-curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/gitVimNORMALorROOT.sh | sh
+wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/gitVimNORMALorROOT.sh -P ~/; bash ~/gitVimNORMALorROOT.sh
+
+## get ssh key and other whatnots
+wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.genericLinuxConfig.sh -P ~/; sh ~/.genericLinuxConfig.sh
