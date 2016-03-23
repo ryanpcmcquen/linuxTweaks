96e2c2829302ab62f07fc9c7668b605a04bd1c07
Ryan P.C. McQuen <ryan.q@linux.com>
2016-03-22 20:30:49 -0700

    update filename

--- debian/.debian8setup.sh
+++ debian/.debian8setup.sh
@@ -5,7 +5,7 @@
 wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.gnome3Setup -P ~/; bash ~/.gnome3Setup
 
 ## retrieve git and vim settings
-wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/gitVimNORMALorROOT.sh -P ~/; bash ~/gitVimNORMALorROOT.sh
+wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.gitVimNORMALorROOT.sh -P ~/; bash ~/.gitVimNORMALorROOT.sh
 
 ## get ssh key and other whatnots
 wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.genericLinuxConfig.sh -P ~/; sh ~/.genericLinuxConfig.sh
