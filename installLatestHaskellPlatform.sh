8e355d7cf2ac70ddb399eb6887536d991b76371f
Ryan P.C. McQuen <ryan.q@linux.com>
2016-04-30 12:01:32 -0700

    Fix curl command.

--- installLatestHaskellPlatform.sh
+++ installLatestHaskellPlatform.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-## curl https://gist.githubusercontent.com/ryanpcmcquen/0521f7dbcf1afdb40429540d3982ed43/raw/installLatestHaskellPlatform.sh | bash
+## curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/installLatestHaskellPlatform.sh | bash
 
 HASKELLDOWNLOAD="$(curl https://www.haskell.org/platform/#linux-generic | grep 'haskell.org/platform/download' | grep 'haskell-platform' | grep 'posix' | cut -d '"' -f2 | sed 's@//@https://@' | tr -d '\n\r')"
 HASKELLVERSION="$(echo $HASKELLDOWNLOAD | cut -d'-' -f3 | tr -d '\n\r')"
