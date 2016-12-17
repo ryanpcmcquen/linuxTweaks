d955e869b5fd78958b5e35aa55fab826b13576a4
Ryan P.C. McQuen <ryan.q@linux.com>
2016-12-17 09:41:39 -0800

    Add settings for newer versions.

--- ubuntu/.unitySetup.sh
+++ ubuntu/.unitySetup.sh
@@ -1,29 +1,38 @@
 #!/bin/bash
+##
 ## wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/ubuntu/.unitySetup.sh -P ~/; bash ~/.unitySetup.sh
 ##
 
-## autohide the dock and set the icons to a smaller, more reasonable size
+## Autohide the dock and set the icons to a smaller, more reasonable size:
 gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-hide-mode 1
 gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ icon-size 32
-## focus follows mouse!
+## Focus follows mouse!
 gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ click-to-focus false
-## 3x3 workspaces
+## 3x3 workspaces:
 gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 3
 gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 3
-## make the keyboard blazingly fast
+## Make the keyboard blazingly fast (old versions):
 gsettings set org.gnome.settings-daemon.peripherals.keyboard repeat true
 gsettings set org.gnome.settings-daemon.peripherals.keyboard delay 150
 gsettings set org.gnome.settings-daemon.peripherals.keyboard repeat-interval 80
-## touchpad settings
+## Make the keyboard blazingly fast (new versions):
+gsettings set org.gnome.desktop.peripherals.keyboard repeat true
+gsettings set org.gnome.desktop.peripherals.keyboard delay 150
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 80
+## Touchpad settings (old versions):
 gsettings set org.gnome.settings-daemon.peripherals.touchpad scroll-method 'two-finger-scrolling'
 gsettings set org.gnome.settings-daemon.peripherals.touchpad horiz-scroll-enabled true
 gsettings set org.gnome.settings-daemon.peripherals.touchpad tap-to-click false
-## show day and date
+## Touchpad settings (new versions):
+gsettings set org.gnome.desktop.peripherals.touchpad scroll-method 'two-finger-scrolling'
+gsettings set org.gnome.desktop.peripherals.touchpad horiz-scroll-enabled true
+gsettings set org.gnome.desktop.peripherals.touchpad tap-to-click false
+## Show day and date:
 gsettings set com.canonical.indicator.datetime show-day true
 gsettings set com.canonical.indicator.datetime show-date true
-## sleep screen after half an hour
+## Sleep screen after half an hour:
 gsettings set org.gnome.desktop.session idle-delay 900
-## fix gvim so it shows up in unity (seems to be fixed upstream)
+## Fix gvim so it shows up in unity (seems to be fixed upstream).
 #sudo cp /usr/share/app-install/desktop/vim-gui-common\:gvim.desktop /usr/share/applications/
-## make gvim take all of gedit's defaults
+## Make gvim take all of gedit's defaults:
 sudo sed -i.bak 's@gedit@gvim@g' /etc/gnome/defaults.list
