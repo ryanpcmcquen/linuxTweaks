dbc6b139b81fb096502ceff37bd785e12bac4b7f
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-04-02 17:56:43 -0700

    Add Emacs config and change name.

--- debian/.debianSetup.sh
+++ debian/.debianSetup.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-## wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/debian/.debian8setup.sh -P ~/; bash ~/.debian8setup.sh
-## basically just gnome and git/vim stuff
+## wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/debian/.debianSetup.sh -P ~/; bash ~/.debianSetup.sh
+## Basically just Gnome and Git/Vim/Emacs stuff.
 
 wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.gnome3Setup -P ~/; bash ~/.gnome3Setup
 
-## retrieve git and vim settings
 wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.gitVimNORMALorROOT.sh -P ~/; bash ~/.gitVimNORMALorROOT.sh
 
-## get ssh key and other whatnots
+wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.emacs -P ~/
+
 wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.genericLinuxConfig.sh -P ~/; sh ~/.genericLinuxConfig.sh
