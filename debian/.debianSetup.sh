984e902e8bac39eb30d38deb16b031ee98cf9d36
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-04-02 18:01:26 -0700

    Remove updates since they would require `sudo`.

--- debian/.debianSetup.sh
+++ debian/.debianSetup.sh
@@ -2,8 +2,9 @@
 ## wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/debian/.debianSetup.sh -P ~/; bash ~/.debianSetup.sh
 ## Basically just Gnome and Git/Vim/Emacs stuff.
 
-sudo apt-get update && sudo apt-get dist-upgrade -y
-sudo apt-get install git curl build-essential vim emacs
+## Uncomment if you set up `sudo`:
+#sudo apt-get update && sudo apt-get dist-upgrade -y
+#sudo apt-get install git curl build-essential vim emacs
 
 wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.gnome3Setup -P ~/; bash ~/.gnome3Setup
 
