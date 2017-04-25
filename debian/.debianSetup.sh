77b557a3fe51880cf550a30d59d290f45590ec8e
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-04-24 19:47:53 -0700

    More packages. More DEs.

--- debian/.debianSetup.sh
+++ debian/.debianSetup.sh
@@ -1,13 +1,26 @@
-#!/bin/sh
+#!/bin/bash
 ## wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/debian/.debianSetup.sh -P ~/; bash ~/.debianSetup.sh
 ## Basically just Gnome and Git/Vim/Emacs stuff.
 
 if [ "`which sudo`" ]; then
   sudo apt-get update && sudo apt-get dist-upgrade -y
-  sudo apt-get install git curl build-essential vim emacs
+  sudo apt-get install -y git curl build-essential vim emacs chromium
 fi
 
-wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.gnome3Setup -P ~/; bash ~/.gnome3Setup
+## Gnome 3 setup!
+if [ `which gnome-shell` ]; then
+  wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.gnome3Setup -P ~/; bash ~/.gnome3Setup
+fi
+
+## Configure KDE if present:
+if [ `which startkde` ]; then
+  wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.kdeSetup.sh -P ~/; bash ~/.kdeSetup.sh
+fi
+
+## Mate!
+if [ `which mate-panel` ]; then
+  wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.mateSetup.sh -P ~/; bash ~/.mateSetup.sh
+fi
 
 wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.gitVimNORMALorROOT.sh -P ~/; bash ~/.gitVimNORMALorROOT.sh
 
