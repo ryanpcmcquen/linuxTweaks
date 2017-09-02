97c88d8b8b586b1f2b400f1177384c4087d2648d
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-09-02 16:50:51 -0700

    Add Textadept. Styling fixes.

--- ubuntu/.ubuntu_LTS_Setup.sh
+++ ubuntu/.ubuntu_LTS_Setup.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-## Run as a normal user:
-## wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/ubuntu/.ubuntu_LTS_Setup.sh -P ~/; bash ~/.ubuntu_LTS_Setup.sh
+# Run as a normal user:
+# wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/ubuntu/.ubuntu_LTS_Setup.sh -P ~/; bash ~/.ubuntu_LTS_Setup.sh
 
-## Tested on 14.04 and 16.04.
+# Tested on 14.04 and 16.04.
 
 sudo apt-get update && sudo apt-get dist-upgrade -y
 
 if [ -z "$(grep 'alias apt' ~/.bash_aliases)" ]; then
-  echo >> ~/.bash_aliases
-  echo "## My aliases:" >> ~/.bash_aliases
-  echo "alias aptu='sudo apt-get update && sudo apt-get dist-upgrade -y'" >> ~/.bash_aliases
-  echo "alias aptc='sudo apt-get autoclean && sudo apt-get clean && sudo apt-get autoremove -y'" >> ~/.bash_aliases
-  echo "alias apti='sudo apt-get install -y '" >> ~/.bash_aliases
+    echo >> ~/.bash_aliases
+    echo "## My aliases:" >> ~/.bash_aliases
+    echo "alias aptu='sudo apt-get update && sudo apt-get dist-upgrade -y'" >> ~/.bash_aliases
+    echo "alias aptc='sudo apt-get autoclean && sudo apt-get clean && sudo apt-get autoremove -y'" >> ~/.bash_aliases
+    echo "alias apti='sudo apt-get install -y '" >> ~/.bash_aliases
 fi
 
 if [ -z "$(grep 'xterm-256color' ~/.bashrc)" ]; then
-  echo >> ~/.bashrc
-  echo 'if [ "$DISPLAY" ]; then' >> ~/.bashrc
-  echo '  export TERM=xterm-256color' >> ~/.bashrc
-  echo 'fi' >> ~/.bashrc
+    echo >> ~/.bashrc
+    echo 'if [ "$DISPLAY" ]; then' >> ~/.bashrc
+    echo '  export TERM=xterm-256color' >> ~/.bashrc
+    echo 'fi' >> ~/.bashrc
 fi
 
 ESSENTIALPKGS="curl wget git vim vim-gnome emacs build-essential libssl-dev lftp httrack chromium-browser exuberant-ctags python3-pip"
@@ -26,68 +26,71 @@ ESSENTIALPKGS="curl wget git vim vim-gnome emacs build-essential libssl-dev lftp
 sudo apt-get install -y $ESSENTIALPKGS
 
 if [ `which unity` ]; then
-  wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/ubuntu/.unitySetup.sh -P ~/; bash ~/.unitySetup.sh
+    wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/ubuntu/.unitySetup.sh -P ~/; bash ~/.unitySetup.sh
 fi
 
-## Only run Gnome 3 setup if Unity is not installed.
+# Only run Gnome 3 setup if Unity is not installed.
 if [ ! `which unity` ] && [ `which gnome-shell` ]; then
-  wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.gnome3Setup -P ~/; bash ~/.gnome3Setup
+    wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.gnome3Setup -P ~/; bash ~/.gnome3Setup
 fi
 
-## Configure KDE if present (Kubuntu):
+# Configure KDE if present (Kubuntu):
 if [ `which startkde` ]; then
-  wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.kdeSetup.sh -P ~/; bash ~/.kdeSetup.sh
+    wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.kdeSetup.sh -P ~/; bash ~/.kdeSetup.sh
 fi
 
-## Mate!
+# Mate!
 if [ `which mate-panel` ]; then
-  wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.mateSetup.sh -P ~/; bash ~/.mateSetup.sh
+    wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.mateSetup.sh -P ~/; bash ~/.mateSetup.sh
 fi
 
-## Pantheon (Elementary OS)!
+# Pantheon (Elementary OS)!
 if [ `which pantheon-greeter` ]; then
-  gsettings set org.gnome.desktop.peripherals.keyboard repeat true
-  gsettings set org.gnome.desktop.peripherals.keyboard repeat 100
-  gsettings set org.gnome.desktop.peripherals.keyboard delay 110
+    gsettings set org.gnome.desktop.peripherals.keyboard repeat true
+    gsettings set org.gnome.desktop.peripherals.keyboard repeat 100
+    gsettings set org.gnome.desktop.peripherals.keyboard delay 110
 fi
 
-## nvm:
+# nvm:
 curl -o- https://raw.githubusercontent.com/creationix/nvm/master/install.sh | bash
 
-## Haskell!
+# Haskell!
 curl -sSL https://get.haskellstack.org/ | sh
 
-## Powerline fonts:
+# Powerline fonts:
 powerline_install() {
-  cd ~/.powerline-fonts/
-  git pull
-  ~/.powerline-fonts/install.sh
+    cd ~/.powerline-fonts/
+    git pull
+    ~/.powerline-fonts/install.sh
 }
 
 if [ -d ~/.powerline-fonts/ ]; then
-  powerline_install
+    powerline_install
 else
-  git clone https://github.com/powerline/fonts.git ~/.powerline-fonts/
-  powerline_install
+    git clone https://github.com/powerline/fonts.git ~/.powerline-fonts/
+    powerline_install
 fi
 cd
 
-## Link back to haskell-vim-now settings in case they were overwritten:
+wget -N https://gist.githubusercontent.com/ryanpcmcquen/655cb3cc60f9d064738903e59504a5fd/raw/installLatestTextadept.sh -P /tmp/ \
+    && bash /tmp/installLatestTextadept.sh
+
+# Link back to haskell-vim-now settings in case they were overwritten:
 #ln -sf ~/.config/haskell-vim-now/.vimrc ~/
 #ln -sf ~/.config/haskell-vim-now/.vim ~/
 
-## Haskell-vim-now!
+# Haskell-vim-now!
 #curl -L https://git.io/haskell-vim-now > /tmp/haskell-vim-now.sh
 #bash /tmp/haskell-vim-now.sh
 
-## Set vim as the default editor:
+# Set vim as the default editor:
 if [ -z "$(grep -r 'EDITOR' /etc/profile.d/ && grep -r 'EDITOR' /etc/profile)" ]; then
-  if [ ! -e /etc/profile.d/vimDefault ]; then
-    sudo sh -c 'echo >> /etc/profile.d/vimDefault'
-    sudo sh -c 'echo "export EDITOR=vim" >> /etc/profile.d/vimDefault'
-    sudo sh -c 'echo "export VISUAL=vim" >> /etc/profile.d/vimDefault'
-    sudo sh -c 'echo >> /etc/profile.d/vimDefault'
-  fi
+    if [ ! -e /etc/profile.d/vimDefault ]; then
+        sudo sh -c 'echo >> /etc/profile.d/vimDefault'
+        sudo sh -c 'echo "export EDITOR=vim" >> /etc/profile.d/vimDefault'
+        sudo sh -c 'echo "export VISUAL=vim" >> /etc/profile.d/vimDefault'
+        sudo sh -c 'echo >> /etc/profile.d/vimDefault'
+    fi
 fi
 
 wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.genericLinuxConfig.sh -P ~/
