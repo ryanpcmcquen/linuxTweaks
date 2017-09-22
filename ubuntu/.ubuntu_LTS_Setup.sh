49d8393b0cf0157e2f631b8a0b7aed1803be6bc8
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-09-22 06:12:09 -0700

    Need the exfat.

--- ubuntu/.ubuntu_LTS_Setup.sh
+++ ubuntu/.ubuntu_LTS_Setup.sh
@@ -28,7 +28,7 @@ xorg-dev libx11-dev libxrandr-dev \
 syslinux-utils libssl-dev lftp httrack \
 chromium-browser exuberant-ctags python3-pip \
 jpegoptim optipng pngquant gifsicle exiv2 \
-vlc scite xfonts-terminus"
+vlc scite xfonts-terminus exfat-fuse exfat-utils"
 
 sudo apt-get install -y $ESSENTIALPKGS
 
