8276f6beef9af1774985c0933d18f4f3a6dd885b
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-10-10 22:28:27 -0700

    Order matters.

--- fedora/.fedoraSetup.sh
+++ fedora/.fedoraSetup.sh
@@ -5,6 +5,15 @@
 # Always good to start with an update.
 sudo dnf -y upgrade || sudo yum -y update
 
+# Pretty sure the first few are included, but it can't hurt.
+ESSENTIALPKGS="curl wget make git vim vim-X11 emacs gcc gcc-c++ \
+clang libXrandr-devel kernel-headers kernel-devel zlib-devel \
+libsndfile-devel SDL-devel scite jpegoptim optipng pngquant \
+gifsicle exiv2"
+
+# If dnf fails, the system is probably older.
+sudo dnf -y install $ESSENTIALPKGS || sudo yum -y install $ESSENTIALPKGS
+
 # Sets up some nice Gnome defaults.
 if [ `which gnome-shell` ]; then
     wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.gnome3Setup -P ~/; bash ~/.gnome3Setup
@@ -29,15 +38,6 @@ fi
 # Sets up: Git, Vim, Emacs, SSH and some nice environment goodies.
 wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.genericLinuxConfig.sh -P ~/
 sh ~/.genericLinuxConfig.sh
-
-# Pretty sure the first few are included, but it can't hurt.
-ESSENTIALPKGS="curl wget make git vim vim-X11 emacs gcc gcc-c++ \
-clang libXrandr-devel kernel-headers kernel-devel zlib-devel \
-libsndfile-devel SDL-devel scite jpegoptim optipng pngquant \
-gifsicle exiv2"
-
-# If dnf fails, the system is probably older.
-sudo dnf -y install $ESSENTIALPKGS || sudo yum -y install $ESSENTIALPKGS
 
 cd
 if [ ! -d ~/.iris/ ]; then
