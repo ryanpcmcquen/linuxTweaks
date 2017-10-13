858a9fa4452b611e02ce9785ed0ada4f1fad60a0
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-10-13 13:57:19 -0700

    Add U2F udev rules. Change indentation.

--- .genericLinuxConfig.sh
+++ .genericLinuxConfig.sh
@@ -1,81 +1,83 @@
 #!/bin/sh
-## Sets up ssh stuff, some bash goodies,
-## and a vimrc, should work on any linux distro.
-## Also Emacs.
+# Sets up ssh stuff, some bash goodies,
+# and a vimrc, should work on any linux distro.
+# Also Emacs.
 
-## wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.genericLinuxConfig.sh -P ~/; sh ~/.genericLinuxConfig.sh
+# wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.genericLinuxConfig.sh -P ~/; sh ~/.genericLinuxConfig.sh
 
-
-## My ssh key:
+# SSH:
 cd
 if [ ! -d ~/.ssh/ ] && [ "`which run_keybase`" ]; then
-  git clone keybase://private/rpcm/ssh ~/.ssh
-  chmod 700 ~/.ssh/
-  chmod 400 ~/.ssh/*
-  chmod 444 ~/.ssh/id_rsa.pub
-  chmod 644 ~/.ssh/known_hosts
+    git clone keybase://private/rpcm/ssh ~/.ssh
+    chmod 700 ~/.ssh/
+    chmod 400 ~/.ssh/*
+    chmod 444 ~/.ssh/id_rsa.pub
+    chmod 644 ~/.ssh/known_hosts
 fi
 cd
 
-## Copy this version if you do not have sudo:
-##echo >> /etc/profile.d/START_SSH_AGENT; echo 'if [ -z "$SSH_AUTH_SOCK" ]; then' >> /etc/profile.d/START_SSH_AGENT; echo '  eval `ssh-agent`' >> /etc/profile.d/START_SSH_AGENT; echo '  ssh-add' >> /etc/profile.d/START_SSH_AGENT; echo 'fi' >> /etc/profile.d/START_SSH_AGENT; echo >> /etc/profile.d/START_SSH_AGENT
+# Copy this version if you do not have sudo:
+#echo >> /etc/profile.d/START_SSH_AGENT; echo 'if [ -z "$SSH_AUTH_SOCK" ]; then' >> /etc/profile.d/START_SSH_AGENT; echo '  eval `ssh-agent`' >> /etc/profile.d/START_SSH_AGENT; echo '  ssh-add' >> /etc/profile.d/START_SSH_AGENT; echo 'fi' >> /etc/profile.d/START_SSH_AGENT; echo >> /etc/profile.d/START_SSH_AGENT
 
-## Start the ssh-agent so we can clone away without
-## entering a passphrase several times:
+# Start the ssh-agent so we can clone away without
+# entering a passphrase several times:
 if [ -z "$SSH_AUTH_SOCK" ]; then
-  eval `ssh-agent`
-  ssh-add
+    eval `ssh-agent`
+    ssh-add
 fi
 
-## Get vimrc, and set up git config:
+# Get vimrc, and set up git config:
 curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.gitVimNORMALorROOT.sh | sh
 
-## Emacs!
+# Emacs!
 wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.emacs -P ~/
 
+# U2F:
+wget -N https://raw.githubusercontent.com/Yubico/libu2f-host/master/70-u2f.rules -P /etc/udev/rules.d/
+
 if [ -e $HOME/.bashrc ]; then
-  if [ -e $HOME/.bash_profile ]; then
-    BASHFILE=".bash_profile"
-  else
-    BASHFILE=".bashrc"
-  fi
-  ## Make ssh less annoying:
-  if [ -z "$(grep 'SSH_AUTH_SOCK' ${BASHFILE})" ]; then
-    echo >> ~/${BASHFILE}
-    echo 'if [ -z "$SSH_AUTH_SOCK" ]; then' >> ~/${BASHFILE}
-    echo '  eval `ssh-agent`' >> ~/${BASHFILE}
-    echo '  ssh-add' >> ~/${BASHFILE}
-    echo 'fi' >> ~/${BASHFILE}
-    echo >> ~/${BASHFILE}
-  fi
-  if [ -z "`grep 'for DIR in' ${BASHFILE}`" ]; then
-    echo >> ~/${BASHFILE}
-    echo '## Extend path with any /opt/ programs:' >> ~/${BASHFILE}
-    echo 'for DIR in /opt/*/bin' >> ~/${BASHFILE}
-    echo '  do PATH="$DIR:$PATH"' >> ~/${BASHFILE}
-    echo 'done' >> ~/${BASHFILE}
-    echo >> ~/${BASHFILE}
-    echo '## Extend path with any /usr/local/ stuff, like Heroku:' >> ~/${BASHFILE}
-    echo 'for DIR in /usr/local/*/bin' >> ~/${BASHFILE}
-    echo '  do PATH="$DIR:$PATH"' >> ~/${BASHFILE}
-    echo 'done' >> ~/${BASHFILE}
-    echo >> ~/${BASHFILE}
-    echo '## Add ~/bin/ to PATH only if it exists, and is not already in $PATH:' >> ~/${BASHFILE}
-    echo '[ -d $HOME/bin/ ] && [ -z "$(echo $PATH | grep $HOME)" ] && PATH=$HOME/bin:"$PATH"' >> ~/${BASHFILE}
-    echo 'PATH=$HOME/.local/bin:"$PATH"' >> ~/${BASHFILE}
-    echo >> ~/${BASHFILE}
-    echo
-    echo "Enjoy your new PATH goodies!"
-    echo
-  else
-    echo
-    echo "You already have the PATH goodies!"
-    echo
-  fi
+    if [ -e $HOME/.bash_profile ]; then
+        BASHFILE=".bash_profile"
+    else
+        BASHFILE=".bashrc"
+    fi
+    # Make ssh less annoying:
+    if [ -z "$(grep 'SSH_AUTH_SOCK' ${BASHFILE})" ]; then
+        echo >> ~/${BASHFILE}
+        echo 'if [ -z "$SSH_AUTH_SOCK" ]; then' >> ~/${BASHFILE}
+        echo '  eval `ssh-agent`' >> ~/${BASHFILE}
+        echo '  ssh-add' >> ~/${BASHFILE}
+        echo 'fi' >> ~/${BASHFILE}
+        echo >> ~/${BASHFILE}
+    fi
+    if [ -z "`grep 'for DIR in' ${BASHFILE}`" ]; then
+        echo >> ~/${BASHFILE}
+        echo '# Extend path with any /opt/ programs:' >> ~/${BASHFILE}
+        echo 'for DIR in /opt/*/bin' >> ~/${BASHFILE}
+        echo '  do PATH="$DIR:$PATH"' >> ~/${BASHFILE}
+        echo 'done' >> ~/${BASHFILE}
+        echo >> ~/${BASHFILE}
+        echo '# Extend path with any /usr/local/ stuff, like Heroku:' >> ~/${BASHFILE}
+        echo 'for DIR in /usr/local/*/bin' >> ~/${BASHFILE}
+        echo '  do PATH="$DIR:$PATH"' >> ~/${BASHFILE}
+        echo 'done' >> ~/${BASHFILE}
+        echo >> ~/${BASHFILE}
+        echo '# Add ~/bin/ to PATH only if it exists, and is not already in $PATH:' >> ~/${BASHFILE}
+        echo '[ -d $HOME/bin/ ] && [ -z "$(echo $PATH | grep $HOME)" ] && PATH=$HOME/bin:"$PATH"' >> ~/${BASHFILE}
+        echo 'PATH=$HOME/.local/bin:"$PATH"' >> ~/${BASHFILE}
+        echo >> ~/${BASHFILE}
+        echo
+        echo "Enjoy your new PATH goodies!"
+        echo
+    else
+        echo
+        echo "You already have the PATH goodies!"
+        echo
+    fi
 else
-  echo
-  echo "You have no .bashrc or .bash_profile, make one like so:"
-  echo "  touch ~/.bashrc"
-  echo
-  exit 1
+    echo
+    echo "You have no .bashrc or .bash_profile, make one like so:"
+    echo "  touch ~/.bashrc"
+    echo
+    exit 1
 fi
