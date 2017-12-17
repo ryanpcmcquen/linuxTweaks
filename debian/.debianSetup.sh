57103b2d2bad43ebd6e925e56fedc213463b7c99
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2017-12-17 15:03:33 -0800

    Add `vlc`.

--- debian/.debianSetup.sh
+++ debian/.debianSetup.sh
@@ -9,7 +9,7 @@ if [ "`groups ${USER} | grep sudo`" ]; then
         git curl wget build-essential gcc make xorg-dev \
         libx11-dev libxrandr-dev vim emacs texlive-latex-extra \
         chromium jpegoptim optipng pngquant gifsicle exiv2 \
-        exfat-fuse exfat-utils scite \
+        exfat-fuse exfat-utils scite vlc \
     "
 
     sudo apt-get update && sudo apt-get dist-upgrade -y
