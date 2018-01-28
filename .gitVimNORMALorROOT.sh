88b8c4e7d3ad1e44284094a94b63bb625fbcd300
Ryan P.C. McQuen <ryanpcmcquen@member.fsf.org>
2018-01-28 12:30:45 -0800

    Cleanup.

--- .gitVimNORMALorROOT.sh
+++ .gitVimNORMALorROOT.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-## as normal or root user
-
+# Run this as any user:
 # curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.gitVimNORMALorROOT.sh | sh
-
 
 GITCONF="https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.gitconfig"
 VIMRC="https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.vimrc"
