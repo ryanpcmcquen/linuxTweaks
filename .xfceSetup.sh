826e93443c64af4284d1d0f03a2eea7da090ebb3
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2018-06-03 14:00:24 -0700

    Remove a lot of stuff. I ride more defaults now.

--- .xfceSetup.sh
+++ .xfceSetup.sh
@@ -2,47 +2,47 @@
 
 # curl https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.xfceSetup.sh | sh
 
-if [ ! `which xfce4-popup-whiskermenu` ]; then
-    # Thanks to @bfanning.
-    # Only one panel!
-    xfconf-query -n -a -c xfce4-panel -p /panels -t int -s 0
-    # This has to match the number of plugins below:
-    xfconf-query --create --channel xfce4-panel --property /panels/panel-0/plugin-ids \
-      --type int --type int --type int --type int --type int --type int --type int \
-      --set 1 --set 2 --set 3 --set 4 --set 5 --set 6 --set 7
-    xfconf-query -n -c xfce4-panel -p /panels/panel-0/length -t int -s 100
-    xfconf-query -n -c xfce4-panel -p /panels/panel-0/size -t int -s 32
-    xfconf-query -n -c xfce4-panel -p /panels/panel-0/position -t string -s "p=6;x=0;y=0"
-    xfconf-query -n -c xfce4-panel -p /panels/panel-0/position-locked -t bool -s true
+# if [ ! `which xfce4-popup-whiskermenu` ]; then
+#     # Thanks to @bfanning.
+#     # Only one panel!
+#     xfconf-query -n -a -c xfce4-panel -p /panels -t int -s 0
+#     # This has to match the number of plugins below:
+#     xfconf-query --create --channel xfce4-panel --property /panels/panel-0/plugin-ids \
+#       --type int --type int --type int --type int --type int --type int --type int \
+#       --set 1 --set 2 --set 3 --set 4 --set 5 --set 6 --set 7
+#     xfconf-query -n -c xfce4-panel -p /panels/panel-0/length -t int -s 100
+#     xfconf-query -n -c xfce4-panel -p /panels/panel-0/size -t int -s 32
+#     xfconf-query -n -c xfce4-panel -p /panels/panel-0/position -t string -s "p=6;x=0;y=0"
+#     xfconf-query -n -c xfce4-panel -p /panels/panel-0/position-locked -t bool -s true
 
-    # Pretty much xfce defaults:
-    xfconf-query -n -c xfce4-panel -p /plugins/plugin-1 -t string -s applicationsmenu
-    xfconf-query -n -c xfce4-panel -p /plugins/plugin-2 -t string -s tasklist
-    xfconf-query -n -c xfce4-panel -p /plugins/plugin-3 -t string -s separator
-    xfconf-query -n -c xfce4-panel -p /plugins/plugin-4 -t string -s pager
-    xfconf-query -n -c xfce4-panel -p /plugins/plugin-5 -t string -s clock
-    xfconf-query -n -c xfce4-panel -p /plugins/plugin-6 -t string -s systray
-    xfconf-query -n -c xfce4-panel -p /plugins/plugin-7 -t string -s pulseaudio
+#     # Pretty much xfce defaults:
+#     xfconf-query -n -c xfce4-panel -p /plugins/plugin-1 -t string -s applicationsmenu
+#     xfconf-query -n -c xfce4-panel -p /plugins/plugin-2 -t string -s tasklist
+#     xfconf-query -n -c xfce4-panel -p /plugins/plugin-3 -t string -s separator
+#     xfconf-query -n -c xfce4-panel -p /plugins/plugin-4 -t string -s pager
+#     xfconf-query -n -c xfce4-panel -p /plugins/plugin-5 -t string -s clock
+#     xfconf-query -n -c xfce4-panel -p /plugins/plugin-6 -t string -s systray
+#     xfconf-query -n -c xfce4-panel -p /plugins/plugin-7 -t string -s pulseaudio
 
-    # Make the top panel beautiful.
-    # Applications menu:
-    xfconf-query -n -c xfce4-panel -p /plugins/plugin-1/show-button-title -t bool -s false
-    # Separator:
-    xfconf-query -n -c xfce4-panel -p /plugins/plugin-3/style -t int -s 0
-    xfconf-query -n -c xfce4-panel -p /plugins/plugin-3/expand -t bool -s true
-    # Window/task list:
-    xfconf-query -n -c xfce4-panel -p /plugins/plugin-2/show-labels -t bool -s false
-    # Workspace pager,
-    # I call this the "Theodore Ts'o" layout:
-    xfconf-query -n -c xfce4-panel -p /plugins/plugin-4/rows -t int -s 3
-    xfconf-query -n -c xfce4-panel -p /plugins/plugin-4/miniature-view -t bool -s true
-    xfconf-query -n -c xfwm4 -p /general/workspace_count -t int -s 9
-    # Clock (fuzzy mode):
-    xfconf-query -n -c xfce4-panel -p /plugins/plugin-5/show-frame -t bool -s false
-    xfconf-query -n -c xfce4-panel -p /plugins/plugin-5/mode -t int -s 3
-    # Systray:
-    xfconf-query -n -c xfce4-panel -p /plugins/plugin-6/show-frame -t bool -s false
-fi
+#     # Make the top panel beautiful.
+#     # Applications menu:
+#     xfconf-query -n -c xfce4-panel -p /plugins/plugin-1/show-button-title -t bool -s false
+#     # Separator:
+#     xfconf-query -n -c xfce4-panel -p /plugins/plugin-3/style -t int -s 0
+#     xfconf-query -n -c xfce4-panel -p /plugins/plugin-3/expand -t bool -s true
+#     # Window/task list:
+#     xfconf-query -n -c xfce4-panel -p /plugins/plugin-2/show-labels -t bool -s false
+#     # Workspace pager,
+#     # I call this the "Theodore Ts'o" layout:
+#     xfconf-query -n -c xfce4-panel -p /plugins/plugin-4/rows -t int -s 3
+#     xfconf-query -n -c xfce4-panel -p /plugins/plugin-4/miniature-view -t bool -s true
+#     xfconf-query -n -c xfwm4 -p /general/workspace_count -t int -s 9
+#     # Clock (fuzzy mode):
+#     xfconf-query -n -c xfce4-panel -p /plugins/plugin-5/show-frame -t bool -s false
+#     xfconf-query -n -c xfce4-panel -p /plugins/plugin-5/mode -t int -s 3
+#     # Systray:
+#     xfconf-query -n -c xfce4-panel -p /plugins/plugin-6/show-frame -t bool -s false
+# fi
 
 # Make window management awesome:
 xfconf-query -n -c xfwm4 -p /general/snap_to_border -t bool -s true
@@ -92,13 +92,13 @@ xfconf-query -n -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Shift><A
 xfconf-query -n -c xfce4-keyboard-shortcuts -p "/xfwm4/custom/<Primary><Shift><Alt>Up" -t string -s "move_window_up_workspace_key"
 
 # Make the desktop tidy.
-xfconf-query -n -c xfce4-desktop -p /desktop-icons/file-icons/show-filesystem -t bool -s false
-xfconf-query -n -c xfce4-desktop -p /desktop-icons/file-icons/show-home -t bool -s false
-xfconf-query -n -c xfce4-desktop -p /desktop-icons/file-icons/show-trash -t bool -s false
+# xfconf-query -n -c xfce4-desktop -p /desktop-icons/file-icons/show-filesystem -t bool -s false
+# xfconf-query -n -c xfce4-desktop -p /desktop-icons/file-icons/show-home -t bool -s false
+# xfconf-query -n -c xfce4-desktop -p /desktop-icons/file-icons/show-trash -t bool -s false
 
 # Remove annoying defaults.
 xfconf-query -n -c xfce4-session -p /general/SaveOnExit -t bool -s false
-xfconf-query -n -c xfce4-session -p /splash/Engine -t string -s mice
+# xfconf-query -n -c xfce4-session -p /splash/Engine -t string -s mice
 
 # Ooh, the power!
 # xfconf-query -n -c xfce4-power-manager -p /xfce4-power-manager/lid-action-on-ac -t int -s 1
@@ -126,28 +126,28 @@ fi
 # fi
 
 # Make the terminal groovier:
-if [ -e ~/.config/xfce4/terminal/terminalrc ]; then
-    #if [ -z "$(grep 'FontName\=Terminus\ 14' ~/.config/xfce4/terminal/terminalrc)" ]; then
-    #    sed -i.bak 's@FontName\=.*@FontName\=Terminus\ 14@g' ~/.config/xfce4/terminal/terminalrc
-    #fi
-    if [ -z "$(grep 'MiscDefaultGeometry\=120x40' ~/.config/xfce4/terminal/terminalrc)" ]; then
-        sed -i.bak 's@MiscDefaultGeometry\=.*@MiscDefaultGeometry\=120x40@g' ~/.config/xfce4/terminal/terminalrc
-    fi
-    if [ -z "$(grep 'BackgroundMode\=TERMINAL_BACKGROUND_TRANSPARENT' ~/.config/xfce4/terminal/terminalrc)" ]; then
-        sed -i.bak 's@BackgroundMode\=.*@BackgroundMode\=TERMINAL_BACKGROUND_TRANSPARENT@g' ~/.config/xfce4/terminal/terminalrc
-    fi
-    if [ -z "$(grep 'BackgroundDarkness\=0.800000' ~/.config/xfce4/terminal/terminalrc)" ]; then
-        sed -i.bak 's@BackgroundDarkness\=.*@BackgroundDarkness\=0.800000@g' ~/.config/xfce4/terminal/terminalrc
-    fi
-    if [ -z "$(grep 'ColorCursor\=#000080000000' ~/.config/xfce4/terminal/terminalrc)" ]; then
-        sed -i.bak 's@ColorCursor\=.*@ColorCursor\=#000080000000@g' ~/.config/xfce4/terminal/terminalrc
-    fi
-else
-    mkdir -pv ~/.config/xfce4/terminal/
-    echo "[Configuration]" >> ~/.config/xfce4/terminal/terminalrc
-    #echo "FontName=Terminus 14" >> ~/.config/xfce4/terminal/terminalrc
-    echo "MiscDefaultGeometry=120x40" >> ~/.config/xfce4/terminal/terminalrc
-    echo "BackgroundMode=TERMINAL_BACKGROUND_TRANSPARENT" >> ~/.config/xfce4/terminal/terminalrc
-    echo "BackgroundDarkness=0.800000" >> ~/.config/xfce4/terminal/terminalrc
-    echo "ColorCursor=#000080000000" >> ~/.config/xfce4/terminal/terminalrc
-fi
+# if [ -e ~/.config/xfce4/terminal/terminalrc ]; then
+#     #if [ -z "$(grep 'FontName\=Terminus\ 14' ~/.config/xfce4/terminal/terminalrc)" ]; then
+#     #    sed -i.bak 's@FontName\=.*@FontName\=Terminus\ 14@g' ~/.config/xfce4/terminal/terminalrc
+#     #fi
+#     if [ -z "$(grep 'MiscDefaultGeometry\=120x40' ~/.config/xfce4/terminal/terminalrc)" ]; then
+#         sed -i.bak 's@MiscDefaultGeometry\=.*@MiscDefaultGeometry\=120x40@g' ~/.config/xfce4/terminal/terminalrc
+#     fi
+#     if [ -z "$(grep 'BackgroundMode\=TERMINAL_BACKGROUND_TRANSPARENT' ~/.config/xfce4/terminal/terminalrc)" ]; then
+#         sed -i.bak 's@BackgroundMode\=.*@BackgroundMode\=TERMINAL_BACKGROUND_TRANSPARENT@g' ~/.config/xfce4/terminal/terminalrc
+#     fi
+#     if [ -z "$(grep 'BackgroundDarkness\=0.800000' ~/.config/xfce4/terminal/terminalrc)" ]; then
+#         sed -i.bak 's@BackgroundDarkness\=.*@BackgroundDarkness\=0.800000@g' ~/.config/xfce4/terminal/terminalrc
+#     fi
+#     if [ -z "$(grep 'ColorCursor\=#000080000000' ~/.config/xfce4/terminal/terminalrc)" ]; then
+#         sed -i.bak 's@ColorCursor\=.*@ColorCursor\=#000080000000@g' ~/.config/xfce4/terminal/terminalrc
+#     fi
+# else
+#     mkdir -pv ~/.config/xfce4/terminal/
+#     echo "[Configuration]" >> ~/.config/xfce4/terminal/terminalrc
+#     #echo "FontName=Terminus 14" >> ~/.config/xfce4/terminal/terminalrc
+#     echo "MiscDefaultGeometry=120x40" >> ~/.config/xfce4/terminal/terminalrc
+#     echo "BackgroundMode=TERMINAL_BACKGROUND_TRANSPARENT" >> ~/.config/xfce4/terminal/terminalrc
+#     echo "BackgroundDarkness=0.800000" >> ~/.config/xfce4/terminal/terminalrc
+#     echo "ColorCursor=#000080000000" >> ~/.config/xfce4/terminal/terminalrc
+# fi
