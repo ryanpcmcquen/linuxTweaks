795a87121c333cc10f56eadf39b388be5465ffdf
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2018-08-02 06:03:47 -0700

    P. C.

--- ubuntu/ubuntu1204Dev64Restore.sh
+++ ubuntu/ubuntu1204Dev64Restore.sh
@@ -24,7 +24,7 @@ sudo apt-get update && sudo apt-get install skype ubuntu-restricted-extras libre
 
 sudo apt-get autoclean && sudo apt-get clean && sudo apt-get autoremove -y
 
-git config --global user.name "Ryan P.C. McQuen"
+git config --global user.name "Ryan P. C. McQuen"
 git config --global user.email "ryanpcmcquen@member.fsf.org"
 git config --global credential.helper cache
 
