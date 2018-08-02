320b96cb0d9b60cbdac948a381e782b114ea4c0a
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2018-08-02 06:01:06 -0700

    P. C.

--- debian/debian7Install64.sh
+++ debian/debian7Install64.sh
@@ -16,7 +16,7 @@ sudo apt-get install curl build-essential linux-headers-amd64 git htop dos2unix 
 ## sudo apt-get install fglrx-driver
 
 
-git config --global user.name "Ryan P.C. McQuen"
+git config --global user.name "Ryan P. C. McQuen"
 git config --global user.email "ryanpcmcquen@member.fsf.org"
 git config --global credential.helper cache
 
