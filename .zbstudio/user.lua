54eb298217e1c7cf1d333e17f783a2607498a536
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-03-20 07:58:23 -0700

    Add ZeroBrane Studio config.

---
+++ .zbstudio/user.lua
@@ -0,0 +1,9 @@
+--[[--
+  Use this file to specify **System** preferences.
+  Review [examples](+/Applications/ZeroBraneStudio.app/Contents/ZeroBraneStudio/cfg/user-sample.lua) or check [online documentation](http://studio.zerobrane.com/documentation.html) for details.
+--]]--
+output.fontname = "IBM Plex Mono"
+output.fontsize = 18
+editor.fontname = "IBM Plex Mono"
+editor.fontsize = 16
+editor.tabwidth = 4
