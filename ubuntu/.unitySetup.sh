04565097a6112bad94205b051e6e342c679d321b
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-05-20 23:52:36 -0700

    Use global key repeat settings.

--- ubuntu/.unitySetup.sh
+++ ubuntu/.unitySetup.sh
@@ -3,6 +3,9 @@
 ## wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/ubuntu/.unitySetup.sh -P ~/; bash ~/.unitySetup.sh
 ##
 
+wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.__KEY_REPEAT_SETTINGS__ -P ~/
+. ~/.__KEY_REPEAT_SETTINGS__
+
 ## Autohide the dock and set the icons to a smaller, more reasonable size:
 gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ launcher-hide-mode 1
 gsettings set org.compiz.unityshell:/org/compiz/profiles/unity/plugins/unityshell/ icon-size 32
@@ -13,12 +16,12 @@ gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ vsize 3
 gsettings set org.compiz.core:/org/compiz/profiles/unity/plugins/core/ hsize 3
 ## Make the keyboard blazingly fast (old versions):
 gsettings set org.gnome.settings-daemon.peripherals.keyboard repeat true
-gsettings set org.gnome.settings-daemon.peripherals.keyboard delay 140
-gsettings set org.gnome.settings-daemon.peripherals.keyboard repeat-interval 100
+gsettings set org.gnome.settings-daemon.peripherals.keyboard delay ${KEY_REPEAT_DELAY}
+gsettings set org.gnome.settings-daemon.peripherals.keyboard repeat-interval ${KEY_REPEAT_RATE}
 ## Make the keyboard blazingly fast (new versions):
 gsettings set org.gnome.desktop.peripherals.keyboard repeat true
-gsettings set org.gnome.desktop.peripherals.keyboard delay 140
-gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval 100
+gsettings set org.gnome.desktop.peripherals.keyboard delay ${KEY_REPEAT_DELAY}
+gsettings set org.gnome.desktop.peripherals.keyboard repeat-interval ${KEY_REPEAT_RATE}
 ## Touchpad settings (old versions):
 gsettings set org.gnome.settings-daemon.peripherals.touchpad scroll-method 'two-finger-scrolling'
 gsettings set org.gnome.settings-daemon.peripherals.touchpad horiz-scroll-enabled true
@@ -35,4 +38,4 @@ gsettings set org.gnome.desktop.session idle-delay 900
 ## Fix gvim so it shows up in unity (seems to be fixed upstream).
 #sudo cp /usr/share/app-install/desktop/vim-gui-common\:gvim.desktop /usr/share/applications/
 ## Make gvim take all of gedit's defaults:
-sudo sed -i.bak 's@gedit@gvim@g' /etc/gnome/defaults.list
+#sudo sed -i.bak 's@gedit@gvim@g' /etc/gnome/defaults.list
