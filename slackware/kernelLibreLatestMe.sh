b7d414d955e558b2cf6be25d42895e0eb2bc9f03
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-07-20 22:21:12 -0700

    Remove license header.

--- slackware/kernelLibreLatestMe.sh
+++ slackware/kernelLibreLatestMe.sh
@@ -10,24 +10,6 @@
 ### sh kernelLibreLatestMe.sh
 ################################
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version, with the following exception:
-# the text of the GPL license may be omitted.
-#
-# This program is distributed in the hope that it will be useful, but
-# without any warranty; without even the implied warranty of
-# merchantability or fitness for a particular purpose. Compiling,
-# interpreting, executing or merely reading the text of the program
-# may result in lapses of consciousness and/or very being, up to and
-# including the end of all existence and the Universe as we know it.
-# See the GNU General Public License for more details.
-#
-# You may have received a copy of the GNU General Public License
-# along with this program (most likely, a file named COPYING). If
-# not, see <https://www.gnu.org/licenses/>.
-#
 # Place this script in /usr/src, chmod +x it to make it executable
 #
 # Initial script by Robby Workman <http://rlworkman.net/>
