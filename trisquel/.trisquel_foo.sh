5445f9676fedaadd738eee6b70677d826bec34d6
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2019-08-26 23:29:09 -0700

    Remove Killr.

--- trisquel/.trisquel_foo.sh
+++ trisquel/.trisquel_foo.sh
@@ -47,11 +47,6 @@ cd
 wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.genericLinuxConfig.sh -P ~/ \
     && sh ~/.genericLinuxConfig.sh
 
-# Killr!
-wget -N https://gist.githubusercontent.com/ryanpcmcquen/7f6b8e1d4f9af71070a0ec59576d4d5b/raw/killr -P ~/ \
-    && sudo install -m755 ~/killr /usr/local/bin/ \
-    && rm ~/killr
-
 # Password generator:
 wget -N https://gist.githubusercontent.com/ryanpcmcquen/a57201e1c8cb31bda6bce9104092725a/raw/password -P ~/ \
     && chmod +x ~/password
