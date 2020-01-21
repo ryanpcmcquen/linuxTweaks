ef1baada9f28575ef2a76f3a803a4e6e530ccf2a
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2020-01-21 14:47:31 -0800

    Do not require confirmations for install.

--- .installSublimeStuff.sh
+++ .installSublimeStuff.sh
@@ -16,28 +16,28 @@ if [ -z "$(which subl)" ]; then
         echo
         curl -O https://download.sublimetext.com/sublimehq-pub.gpg && sudo pacman-key --add sublimehq-pub.gpg && sudo pacman-key --lsign-key 8A8F901A && rm sublimehq-pub.gpg
         echo -e "\n[sublime-text]\nServer = https://download.sublimetext.com/arch/stable/x86_64" | sudo tee -a /etc/pacman.conf
-        sudo pacman -Syu sublime-text sublime-merge
+        sudo pacman -Syyu sublime-text sublime-merge
     elif [ "$(which dnf)" ]; then
         echo
         echo "Package manager detected: dnf"
         echo
         sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
         sudo dnf config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-        sudo dnf install sublime-text sublime-merge
+        sudo dnf -y install sublime-text sublime-merge
     elif [ "$(which yum)" ]; then
         echo
         echo "Package manager detected: yum"
         echo
         sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
         sudo yum-config-manager --add-repo https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-        sudo yum install sublime-text sublime-merge
+        sudo yum -y install sublime-text sublime-merge
     elif [ "$(which zypper)" ]; then
         echo
         echo "Package manager detected: zypper"
         echo
         sudo rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
         sudo zypper addrepo -g -f https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
-        sudo zypper install sublime-text sublime-merge
+        sudo zypper install -y sublime-text sublime-merge
     else
         echo
         echo "No compatible package manager found. Please download the tarball here:"
