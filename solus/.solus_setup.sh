5402361afcbf0454888c70613ef4103cbcfdc97c
Ryan P. C. McQuen <rpcm@linux.com>
2020-02-27 15:28:24 -0800

    Add a Solus provisioning script.

---
+++ solus/.solus_setup.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+# wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/solus/.solus_setup.sh -P ~/ && bash ~/.solus_setup.sh
+
+sudo eopkg upgrade
+sudo eopkg install -y -c system.devel
+sudo eopkg install -y \
+    cloc emacs fuse git keybase linux-lts llvm llvm-clang openssh \
+    sdl2-devel sdl2-image-devel sdl2-mixer-devel sdl2-ttf-devel
+sudo usermod -aG fuse $(whoami)
+snap refresh
+snap install sublime-text --classic
+snap install sublime-merge --classic
+
+wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.genericLinuxConfig.sh -P ~/ && \
+    bash ~/.genericLinuxConfig.sh
+
+if [ "`which plasmashell`" ]; then
+    wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.kdeSetup.sh -P ~/ && \
+        bash .kdeSetup.sh
+fi
