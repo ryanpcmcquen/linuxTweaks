6fe34d438dbb9867193b4ce27f2a677d66994bc3
Ryan P. C. McQuen <rpcm@linux.com>
2020-03-15 13:18:37 -0700

    Decrease font size.

--- .zbstudio/user.lua
+++ .zbstudio/user.lua
@@ -3,9 +3,9 @@
   Review [examples](+/Applications/ZeroBraneStudio.app/Contents/ZeroBraneStudio/cfg/user-sample.lua) or check [online documentation](http://studio.zerobrane.com/documentation.html) for details.
 --]]--
 output.fontname = "IBM Plex Mono"
-output.fontsize = 18
+output.fontsize = 12
 editor.fontname = "IBM Plex Mono"
-editor.fontsize = 16
+editor.fontsize = 11
 editor.tabwidth = 4
 
 -- Correct indentation on save (autoformat).
