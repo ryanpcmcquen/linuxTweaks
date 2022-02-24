6da955fcbec04ffc1c272774f36a448f35d8f507
Ryan McQuen <rpcm@linux.com>
2022-02-24 15:43:42 -0800

    Install Textadept.

--- debian/.debianSetup.sh
+++ debian/.debianSetup.sh
@@ -47,7 +47,8 @@ if [ "`groups ${USER} | grep sudo`" ]; then
     # SciTE configuration:
     wget -N https://raw.githubusercontent.com/ryanpcmcquen/linuxTweaks/master/.SciTEUser.properties -P ~/
 
-    wget -N https://gist.githubusercontent.com/ryanpcmcquen/655cb3cc60f9d064738903e59504a5fd/raw/.installLatestTextadept.sh -P ~/
+    wget -N https://gist.githubusercontent.com/ryanpcmcquen/655cb3cc60f9d064738903e59504a5fd/raw/.installLatestTextadept.sh -P ~/ \
+        && bash ~/.installLatestTextadept.sh
 
     # Keybase:
     cd
